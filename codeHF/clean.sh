3b9cf194cc7a0a602deb57454dac0f3b4ce40abf
Vít Kučera <vit.kucera@cern.ch>
2020-08-06 15:01:07 +0200

    Delete only created files.

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -2,11 +2,10 @@
 
 echo -e "\nCleaning"
 
-rm -f *.root
+rm -f AO2D.root AnalysisResults.root EventStat_temp.root Vertices2prong-ITS1*.root event_stat.root
 rm -f *.txt
-rm -f *.pdf
+rm -f comparison.pdf
 rm -f *.log
 rm -f output*
 rm -f localhost*
 rm -f dpl-config.json
-git checkout -- comparison_ref.pdf
