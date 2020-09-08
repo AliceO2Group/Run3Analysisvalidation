b237c8ec22212d934d4b35514fff9373dd851726
Gian Michele Innocenti <ginnocen@cern.ch>
2020-09-08 10:56:17 +0200

    Fix

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -3,7 +3,6 @@
 echo -e "\nCleaning"
 
 rm -f AO2D.root AnalysisResults.root AnalysisResultsQA.root EventStat_temp.root Vertices2prong-ITS1*.root event_stat.root
-rm -f *.txt
 rm -f comparison.pdf
 rm -f *.log
 rm -f output*
