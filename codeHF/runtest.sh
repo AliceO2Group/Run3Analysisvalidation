669690bc3e1ab235519504e1497c6d682c038d6d
Vít Kučera <vit.kucera@cern.ch>
2020-09-18 19:35:01 +0200

    Simplify settings.

--- codeHF/runtest.sh
+++ codeHF/runtest.sh
@@ -15,84 +15,48 @@ CONVSEP=1     # Convert ESD files separately.
 PARALLELISE=0 # Parallelise O2 tasks.
 TWOPRONGSEL=0 # Apply D0 selection cuts.
 
+# Default settings
+JSON="$PWD/dpl-config_std.json"
+ISMC=0
+MASS=1.8
+TRIGGERSTRINGRUN2=""
+TRIGGERBITRUN3=-1
+NMAX=-1
+
+if [ $TWOPRONGSEL -eq 1 ]; then
+  JSON="$PWD/dpl-config_selection.json"
+fi
+
 if [ $CASE -eq 0 ]; then
   INPUTDIR="../twikiinput"
-  ISMC=0
-  LISTNAME="listprodhfrun3_ppK0starToyMC.txt"
-  AOD3NAME=AO2D.root
   MASS=1.0
   STRING="AliESDs_ppK0starToyMC.root"
-  TRIGGERSTRINGRUN2=""
-  TRIGGERBITRUN3=-1
-  NMAX=-1
-  JSON=dpl-config_std.json
-  if [ $TWOPRONGSEL -eq 1 ]; then
-    JSON=dpl-config_selection.json
-  fi
 fi
 
 if [ $CASE -eq 1 ]; then
   INPUTDIR="/mnt/temp/Run3data/data/LHC15o_246751/pass1"
-  ISMC=0
-  LISTNAME="listprodhfrun3_PbPb_data_LHC15o_246751.txt"
-  AOD3NAME=AO2D.root
-  MASS=1.8
   STRING="15000246751019.110/AliESDs.root"
   TRIGGERSTRINGRUN2="CV0L7-B-NOPF-CENT"
   TRIGGERBITRUN3=5 #FIXME
   NMAX=5
-  JSON=dpl-config_std.json
-  if [ $TWOPRONGSEL -eq 1 ]; then
-    JSON=dpl-config_selection.json
-  fi
 fi
 
 if [ $CASE -eq 2 ]; then
   INPUTDIR="/data/Run3data/alice_sim_2015_LHC15k1a3_246391/246391"
   ISMC=1
-  LISTNAME="listprodhfrun3_mc_HIJING_PbPb_LHC15k1a3.txt"
-  AOD3NAME=AO2D.root
-  MASS=1.8
   STRING="00*/AliESDs.root"
-  TRIGGERSTRINGRUN2=""
-  TRIGGERBITRUN3=-1
   NMAX=1
-  JSON=dpl-config_std.json
-  if [ $TWOPRONGSEL -eq 1 ]; then
-    JSON=dpl-config_selection.json
-  fi
 fi
 
 if [ $CASE -eq 3 ]; then
   INPUTDIR="/data/Run3data/output"
-  ISMC=0
-  LISTNAME="listprodhfrun3_K0sMC.txt"
-  AOD3NAME=AO2D.root
   MASS=1.0
   STRING="00*/AliESDs.root"
-  TRIGGERSTRINGRUN2=""
-  TRIGGERBITRUN3=-1
-  NMAX=-1
-  JSON=dpl-config_std.json
-  if [ $TWOPRONGSEL -eq 1 ]; then
-    JSON=dpl-config_selection.json
-  fi
 fi
 
 if [ $CASE -eq 4 ]; then
   INPUTDIR="/data/Run3data/alice_sim_2018_LHC18a4a2_cent/282099"
-  ISMC=0
-  LISTNAME="listprodhfrun3_mc_pp_D2H_LHC18a4a2_cent.txt"
-  AOD3NAME=AO2D.root
-  MASS=1.8
   STRING="001/AliESDs.root"
-  TRIGGERSTRINGRUN2=""
-  TRIGGERBITRUN3=-1
-  NMAX=-1
-  JSON=dpl-config_std.json
-  if [ $TWOPRONGSEL -eq 1 ]; then
-    JSON=dpl-config_selection.json
-  fi
 fi
 
 #INPUTDIR="/data/Run3data/output" #K0* MC injected
