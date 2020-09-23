85cd418ffd4ac60e5f5a389a967206481a90876a
Vít Kučera <vit.kucera@cern.ch>
2020-09-23 06:20:47 +0200

    Update cleaning script.

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -2,11 +2,7 @@
 
 echo -e "\nCleaning"
 
-rm -f AO2D*.root AnalysisResults.root AnalysisResultsQA.root EventStat_temp.root Vertices2prong-ITS1*.root event_stat.root
-rm -f comparison_histos.pdf comparison_ratios.pdf
+rm -f AO2D.root AnalysisResults.root AnalysisResultsQA.root Vertices2prong-ITS1.root
+rm -f comparison_histos_*prong.pdf comparison_ratios_*prong.pdf
 rm -f *.log
 rm -rf output_*
-rm -f output*
-rm -f *_sel.json
-rm -f localhost*
-rm -f dpl-config.json
