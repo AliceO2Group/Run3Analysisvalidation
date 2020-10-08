5013c9af305d670f41571254dd518be3049c255c
Francesco Prino <prino@to.infn.it>
2020-10-08 12:40:19 +0200

    Macro to run the AliPhysicsTask locally

---
+++ codeHF/RunHFTaskLocal.C
@@ -0,0 +1,46 @@
+
+void RunHFTaskLocal(TString esdfilename = "./AliESDs.root",
+		    TString jsonfilename = "dpl-config_std.json",
+		    Bool_t isMC=kFALSE){
+
+  // Load common libraries
+  gSystem->Load("libCore.so");  
+  gSystem->Load("libTree.so");
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libPhysics.so");
+  gSystem->Load("libSTEERBase");
+  gSystem->Load("libESD");
+  gSystem->Load("libAOD");
+  gSystem->Load("libANALYSIS");
+  gSystem->Load("libANALYSISalice");   
+  gSystem->SetIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_ROOT/ITS -I$ALICE_ROOT/TPC -I$ALICE_ROOT/CONTAINERS -I$ALICE_ROOT/STEER/STEER -I$ALICE_ROOT/STEER/STEERBase -I$ALICE_ROOT/STEER/ESD -I$ALICE_ROOT/STEER/CDB -I$ALICE_ROOT/TRD -I$ALICE_ROOT/macros -I$ALICE_ROOT/ANALYSIS -I$ALICE_PHYSICS -I$ALICE_PHYSICS/include -g"); 
+  
+  AliAnalysisManager *mgr = new AliAnalysisManager("testAnalysis");
+  
+  TChain *chainESD = new TChain("esdTree");
+  chainESD->Add(esdfilename.Data());
+
+  // Create and configure the alien handler plugin
+  AliESDInputHandler* esdH = new AliESDInputHandler();
+  //  esdH->SetNeedField(kTRUE);
+  mgr->SetInputEventHandler(esdH);
+
+  AliMCEventHandler* handler = new AliMCEventHandler;
+  handler->SetReadTR(kFALSE);
+  mgr->SetMCtruthEventHandler(handler);
+
+  //Apply the event selection
+  gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
+  
+  AliPhysicsSelectionTask *physSelTask = reinterpret_cast<AliPhysicsSelectionTask*>(gInterpreter->ProcessLine(Form(".x %s(%d)",gSystem->ExpandPathName("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C"),isMC)));
+
+
+  AliAnalysisTaskHFSimpleVertices* tasktr3 = reinterpret_cast<AliAnalysisTaskHFSimpleVertices*>(gInterpreter->ProcessLine(Form(".x %s(\"\",\"%s\")",gSystem->ExpandPathName("$ALICE_PHYSICS/PWGHF/vertexingHF/macros/AddTaskHFSimpleVertices.C"),jsonfilename.Data())));
+
+  mgr->InitAnalysis();
+  mgr->PrintStatus();
+  mgr->StartAnalysis("local",chainESD);
+};
+
+
