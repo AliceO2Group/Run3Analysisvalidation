4dbecb39bd78535609b6925d502df969d6ca19e8
Vít Kučera <vit.kucera@cern.ch>
2020-10-30 05:45:30 +0100

    Include the QA task in the main O2 section.

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -7,7 +7,7 @@ function MsgErr { echo -e "\e[1;31m$@\e[0m"; }
 
 MsgStep "Cleaning"
 
-rm -f AnalysisResults_ALI_HF.root AnalysisResults_O2_HF.root AnalysisResults_O2_QA.root
+rm -f AnalysisResults_ALI.root AnalysisResults_O2.root
 rm -f comparison_histos_2prong.pdf comparison_ratios_2prong.pdf comparison_histos_3prong.pdf comparison_ratios_3prong.pdf
 rm -f *.log
 rm -rf output_*
