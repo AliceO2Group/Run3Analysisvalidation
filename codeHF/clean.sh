bf5121bb711cf1c98c24a74bd1a2b8a5797df267
Vít Kučera <vit.kucera@cern.ch>
2020-11-05 16:39:44 +0100

    Add switch for Λc cuts.

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -11,3 +11,5 @@ rm -f AnalysisResults_ALI.root AnalysisResults_O2.root AnalysisResults_trees_O2.
 rm -f comparison_histos_2prong.pdf comparison_ratios_2prong.pdf comparison_histos_3prong.pdf comparison_ratios_3prong.pdf && \
 rm -f *.log && \
 rm -rf output_* || { MsgErr "Error"; exit 1; }
+
+exit 0
