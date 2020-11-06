71d839a452589113e4eca5ea563c1baa46ae7ae0
Vít Kučera <vit.kucera@cern.ch>
2020-11-06 17:32:40 +0100

    Make input cases more compact.

--- codeHF/runtest.sh
+++ codeHF/runtest.sh
@@ -46,64 +46,49 @@ TRIGGERSTRINGRUN2=""                  # Run 2 trigger (not used)
 TRIGGERBITRUN3=-1                     # Run 3 trigger (not used)
 
 # Input specification
-if [ $INPUT_CASE -eq 0 ]; then
-  INPUT_LABEL="ppK0starToyMC"
-  INPUT_DIR="../twikiinput"
-  INPUT_FILES="AliESDs_ppK0starToyMC.root"
-  MASS=1.0
-fi
-
-if [ $INPUT_CASE -eq 1 ]; then
-  INPUT_LABEL="Pb-Pb real LHC15o"
-  INPUT_DIR="/mnt/temp/Run3data/data/LHC15o_246751/pass1"
-  TRIGGERSTRINGRUN2="CV0L7-B-NOPF-CENT"
-  TRIGGERBITRUN3=5 #FIXME
-fi
-
-if [ $INPUT_CASE -eq 2 ]; then
-  INPUT_LABEL="Pb-Pb MC LHC15o"
-  INPUT_DIR="/data/Run3data/alice_sim_2015_LHC15k1a3_246391/246391"
-  ISMC=1
-fi
-
-if [ $INPUT_CASE -eq 3 ]; then
-  INPUT_LABEL="?"
-  INPUT_DIR="/data/Run3data/output"
-  MASS=1.0
-fi
-
-if [ $INPUT_CASE -eq 4 ]; then
-  INPUT_LABEL="p-p MC LHC17p"
-  INPUT_DIR="/data/Run3data/alice_sim_2018_LHC18a4a2_cent/282099"
-  ISMC=1
-fi
-
-if [ $INPUT_CASE -eq 5 ]; then
-  INPUT_LABEL="p-p MC LHC17p"
-  INPUT_DIR="/mnt/temp/Run3data_Vit/LHC18a4a2_cent/282341"
-  ISMC=1
-fi
-
-if [ $INPUT_CASE -eq 6 ]; then
-  INPUT_LABEL="p-p real LHC17p"
-  INPUT_DIR="/mnt/temp/Run3data_Vit/LHC17p_pass1_CENT_woSDD/282341"
-fi
-
-if [ $INPUT_CASE -eq 7 ]; then
-  INPUT_LABEL="Pb-Pb real LHC15o, converted (AliHyperloop LHC15o_test sample)"
-  INPUT_DIR="/mnt/temp/Run3data_Vit/LHC15o_converted/alice/data/2015/LHC15o/000244918/pass5_lowIR/PWGZZ/Run3_Conversion/96_20201013-1346_child_1/"
-  INPUT_FILES="*/AO2D.root"
-  ISINPUTO2=1
-fi
-
-if [ $INPUT_CASE -eq 8 ]; then
-  INPUT_LABEL="Run 5, p-p MC 14 TeV MB"
-  INPUT_DIR="/data/Run5data/MB_100kev_100cmdefault_05112020"
-  INPUT_FILES="AODRun5.*.root"
-  JSON="$JSONRUN5"
-  ISINPUTO2=1
-  ISMC=1
-fi
+case $INPUT_CASE in
+  0)
+    INPUT_LABEL="ppK0starToyMC"
+    INPUT_DIR="../twikiinput"
+    INPUT_FILES="AliESDs_ppK0starToyMC.root"
+    MASS=1.0;;
+  1)
+    INPUT_LABEL="Pb-Pb real LHC15o"
+    INPUT_DIR="/mnt/temp/Run3data/data/LHC15o_246751/pass1"
+    TRIGGERSTRINGRUN2="CV0L7-B-NOPF-CENT"
+    TRIGGERBITRUN3=5;; #FIXME
+  2)
+    INPUT_LABEL="Pb-Pb MC LHC15o"
+    INPUT_DIR="/data/Run3data/alice_sim_2015_LHC15k1a3_246391/246391"
+    ISMC=1;;
+  3)
+    INPUT_LABEL="?"
+    INPUT_DIR="/data/Run3data/output"
+    MASS=1.0;;
+  4)
+    INPUT_LABEL="p-p MC LHC17p"
+    INPUT_DIR="/data/Run3data/alice_sim_2018_LHC18a4a2_cent/282099"
+    ISMC=1;;
+  5)
+    INPUT_LABEL="p-p MC LHC17p"
+    INPUT_DIR="/mnt/temp/Run3data_Vit/LHC18a4a2_cent/282341"
+    ISMC=1;;
+  6)
+    INPUT_LABEL="p-p real LHC17p"
+    INPUT_DIR="/mnt/temp/Run3data_Vit/LHC17p_pass1_CENT_woSDD/282341";;
+  7)
+    INPUT_LABEL="Pb-Pb real LHC15o, converted (AliHyperloop LHC15o_test sample)"
+    INPUT_DIR="/mnt/temp/Run3data_Vit/LHC15o_converted/alice/data/2015/LHC15o/000244918/pass5_lowIR/PWGZZ/Run3_Conversion/96_20201013-1346_child_1/"
+    INPUT_FILES="*/AO2D.root"
+    ISINPUTO2=1;;
+  8)
+    INPUT_LABEL="Run 5, p-p MC 14 TeV MB"
+    INPUT_DIR="/data/Run5data/MB_100kev_100cmdefault_05112020"
+    INPUT_FILES="AODRun5.*.root"
+    JSON="$JSONRUN5"
+    ISINPUTO2=1
+    ISMC=1;;
+esac
 
 # Message formatting
 function MsgStep { echo -e "\n\e[1;32m$@\e[0m"; }
