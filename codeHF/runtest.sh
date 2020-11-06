25ed45647e28f6ab68e73931d5edb42e00473c63
Vít Kučera <vit.kucera@cern.ch>
2020-11-06 17:55:56 +0100

    Add notes for broken input cases.

--- codeHF/runtest.sh
+++ codeHF/runtest.sh
@@ -47,21 +47,21 @@ TRIGGERBITRUN3=-1                     # Run 3 trigger (not used)
 
 # Input specification
 case $INPUT_CASE in
-  0)
+  0) # FIXME: missing input files
     INPUT_LABEL="ppK0starToyMC"
     INPUT_DIR="../twikiinput"
     INPUT_FILES="AliESDs_ppK0starToyMC.root"
     MASS=1.0;;
-  1)
+  1) # FIXME: no read permission
     INPUT_LABEL="Pb-Pb real LHC15o"
     INPUT_DIR="/mnt/temp/Run3data/data/LHC15o_246751/pass1"
     TRIGGERSTRINGRUN2="CV0L7-B-NOPF-CENT"
     TRIGGERBITRUN3=5;; #FIXME
-  2)
+  2) # FIXME: conversion fails
     INPUT_LABEL="Pb-Pb MC LHC15o"
     INPUT_DIR="/data/Run3data/alice_sim_2015_LHC15k1a3_246391/246391"
     ISMC=1;;
-  3)
+  3) # FIXME: conversion fails
     INPUT_LABEL="?"
     INPUT_DIR="/data/Run3data/output"
     MASS=1.0;;
@@ -78,7 +78,7 @@ case $INPUT_CASE in
     INPUT_DIR="/mnt/temp/Run3data_Vit/LHC17p_pass1_CENT_woSDD/282341";;
   7)
     INPUT_LABEL="Pb-Pb real LHC15o, converted (AliHyperloop LHC15o_test sample)"
-    INPUT_DIR="/mnt/temp/Run3data_Vit/LHC15o_converted/alice/data/2015/LHC15o/000244918/pass5_lowIR/PWGZZ/Run3_Conversion/96_20201013-1346_child_1/"
+    INPUT_DIR="/mnt/temp/Run3data_Vit/LHC15o_converted/alice/data/2015/LHC15o/000244918/pass5_lowIR/PWGZZ/Run3_Conversion/96_20201013-1346_child_1"
     INPUT_FILES="*/AO2D.root"
     ISINPUTO2=1;;
   8)
