e59b85220306a20aa171814ea09a0d5e28162913
Vít Kučera <vit.kucera@cern.ch>
2020-11-16 00:44:27 +0100

    Add more debugging messages.

--- codeHF/batch_ali.sh
+++ codeHF/batch_ali.sh
@@ -2,14 +2,16 @@
 
 # Script to run AliPhysics tasks in jobs.
 
-# Load utilities.
-source utilities.sh || ErrExit "Failed to load utilities."
-
 LISTINPUT="$1"
 JSON="$2"
 SCRIPT="$3"
 DEBUG=$4
 FILEOUT="AnalysisResults.root"
+
+[ $DEBUG -eq 1 ] && echo "Running $0"
+
+# Load utilities.
+source utilities.sh || ErrExit "Failed to load utilities."
 
 CheckFile "$SCRIPT"
 CheckFile "$JSON"
