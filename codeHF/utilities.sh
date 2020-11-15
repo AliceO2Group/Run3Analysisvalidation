514a909b4f3ceb6f5c7cf0d0b1862057037460df
Vít Kučera <vit.kucera@cern.ch>
2020-11-15 20:57:36 +0100

    Move formatting to utilities.

--- codeHF/utilities.sh
+++ codeHF/utilities.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Message formatting
+# Utilities
 
+# Message formatting
 function MsgStep { echo -e "\n\e[1;32m$@\e[0m"; }
 function MsgSubStep { echo -e "\e[1m$@\e[0m"; }
 function MsgWarn { echo -e "\e[1;36m$@\e[0m"; }
