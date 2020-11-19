8ed066c5ee982c38bf3f5745a1bc3d623eb42b37
Vít Kučera <vit.kucera@cern.ch>
2020-11-19 05:31:33 +0100

    Add dry run and command line options.

--- config/config_update.sh
+++ config/config_update.sh
@@ -14,7 +14,7 @@ CLEAN=1
 PURGE_BUILDS=0
 
 # Print out an overview of the latest commits of repositories.
-PRINT_COMMITS=0
+PRINT_COMMITS=1
 
 # Main ALICE software directory
 #ALICE_DIR="$HOME/alice"
@@ -30,7 +30,7 @@ PRINT_COMMITS=0
 
 # alidist
 ALIDIST_NAME="alidist"
-ALIDIST_UPDATE=1
+ALIDIST_UPDATE=0
 ALIDIST_DIR="$ALICE_DIR/alidist"
 ALIDIST_REMOTE_MAIN="upstream"
 ALIDIST_REMOTE_FORK=""
@@ -39,13 +39,13 @@ ALIDIST_SPECS=("$ALIDIST_NAME" $ALIDIST_UPDATE "$ALIDIST_DIR" "$ALIDIST_REMOTE_M
 
 # AliPhysics
 ALIPHYSICS_NAME="AliPhysics"
-ALIPHYSICS_UPDATE=1
+ALIPHYSICS_UPDATE=0
 ALIPHYSICS_DIR="$ALICE_DIR/AliPhysics"
 ALIPHYSICS_REMOTE_MAIN="upstream"
 ALIPHYSICS_REMOTE_FORK="origin"
 ALIPHYSICS_BRANCH_MAIN="master"
 ALIPHYSICS_BUILD_OPT="--defaults user-next-root6"
-ALIPHYSICS_BUILD=1
+ALIPHYSICS_BUILD=0
 ALIPHYSICS_SPECS=("$ALIPHYSICS_NAME" $ALIPHYSICS_UPDATE "$ALIPHYSICS_DIR" "$ALIPHYSICS_REMOTE_MAIN" "$ALIPHYSICS_REMOTE_FORK" "$ALIPHYSICS_BRANCH_MAIN" "$ALIPHYSICS_BUILD_OPT" $ALIPHYSICS_BUILD)
 
 # O2
@@ -62,7 +62,7 @@ O2_SPECS=("$O2_NAME" $O2_UPDATE "$O2_DIR" "$O2_REMOTE_MAIN" "$O2_REMOTE_FORK" "$
 # Run 3 validation
 RUN3VALIDATE_NAME="Run 3 validation"
 RUN3VALIDATE_UPDATE=1
-RUN3VALIDATE_DIR="$(dirname $(realpath $0))"
+RUN3VALIDATE_DIR="$DIR_REPO"
 RUN3VALIDATE_REMOTE_MAIN="upstream"
 RUN3VALIDATE_REMOTE_FORK="origin"
 RUN3VALIDATE_BRANCH_MAIN="master"
@@ -70,12 +70,18 @@ RUN3VALIDATE_SPECS=("$RUN3VALIDATE_NAME" $RUN3VALIDATE_UPDATE "$RUN3VALIDATE_DIR
 
 ####################################################################################################
 
-# List of packages to update/build. (Put alidist first!)
+# List of packages to update/build (Put alidist first!)
 LIST_PKG_SPECS=(
-#"ALIDIST_SPECS"
-#"ALIPHYSICS_SPECS"
+"ALIDIST_SPECS"
+"ALIPHYSICS_SPECS"
 "O2_SPECS"
 "RUN3VALIDATE_SPECS"
+)
+
+# List of development aliBuild packages
+LIST_PKG_DEV_SPECS=(
+"ALIPHYSICS_SPECS"
+"O2_SPECS"
 )
 
 ####################################################################################################
