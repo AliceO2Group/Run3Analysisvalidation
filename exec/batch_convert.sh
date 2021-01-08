c5927edeb5c4d236e10721e9dfccd9b09287022c
Vít Kučera <vit.kucera@cern.ch>
2021-01-08 02:21:09 +0100

    Check existence of list of scripts.

--- exec/batch_convert.sh
+++ exec/batch_convert.sh
@@ -50,6 +50,7 @@ while read FileIn; do
   ((IndexFile+=1))
 done < "$LISTINPUT"
 
+CheckFile "$ListRunScripts"
 echo "Running conversion jobs... ($(cat $ListRunScripts | wc -l) jobs)"
 OPT_PARALLEL="--halt soon,fail=100%"
 if [ $DEBUG -eq 0 ]; then
