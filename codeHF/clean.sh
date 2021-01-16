7c0a9c70bd079e386da89ac9d90f5ac4a96319f7
Vít Kučera <vit.kucera@cern.ch>
2021-01-16 07:49:57 +0100

    Update cleaning script.

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -4,7 +4,9 @@
 
 rm -rf \
 AnalysisResults_ALI.root AnalysisResults_O2.root AnalysisResults_trees_O2.root \
-comparison_histos_2prong.pdf comparison_ratios_2prong.pdf comparison_histos_3prong.pdf comparison_ratios_3prong.pdf \
+comparison_histos_skim.pdf comparison_histos_tracks.pdf comparison_ratios_skim.pdf comparison_ratios_tracks.pdf \
+comparison_histos_cand2.pdf comparison_histos_cand3.pdf comparison_ratios_cand2.pdf comparison_ratios_cand3.pdf \
+comparison_histos_d0.pdf comparison_histos_dplus.pdf comparison_ratios_d0.pdf comparison_ratios_dplus.pdf \
 D0_MC_eff.pdf  D0_MC_pT.pdf \
 ./*.log \
 output_* \
