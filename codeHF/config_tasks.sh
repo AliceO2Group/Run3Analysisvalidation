15e5e792269261b5ecb0544b81cf383f30ffb5f1
Gian Michele Innocenti <ginnocen@cern.ch>
2021-01-31 13:44:14 +0100

    Add Bplus task for data only

--- codeHF/config_tasks.sh
+++ codeHF/config_tasks.sh
@@ -37,6 +37,7 @@ DOO2_TASK_D0=1      # hf-task-d0
 DOO2_TASK_DPLUS=1   # hf-task-dplus
 DOO2_TASK_LC=1      # hf-task-lc
 DOO2_TASK_JPSI=1    # hf-task-jpsi
+DOO2_TASK_BPLUS=1   # hf-task-bplus
 # Selection cuts
 APPLYCUTS_D0=0      # Apply D0 selection cuts.
 APPLYCUTS_LC=0      # Apply Λc selection cuts.
@@ -97,6 +98,7 @@ function MakeScriptO2 {
   [ $DOO2_TASK_D0 -eq 1 ] && { DOO2_SEL_D0=1; }
   [ $DOO2_SEL_D0 -eq 1 ] && { DOO2_CAND_2PRONG=1; DOO2_PID_TPC=1; DOO2_PID_TOF=1; }
   [ $DOO2_TASK_JPSI -eq 1 ] && { DOO2_SEL_JPSI=1; }
+  [ $DOO2_TASK_BPLUS -eq 1 ] && { DOO2_SEL_D0=1; }
   [ $DOO2_SEL_JPSI -eq 1 ] && { DOO2_CAND_2PRONG=1; DOO2_PID_TPC=1; DOO2_PID_TOF=1; }
   [ $DOO2_CAND_2PRONG -eq 1 ] && { DOO2_SKIM=1; }
   [ $DOO2_TASK_DPLUS -eq 1 ] && { DOO2_CAND_3PRONG=1; }
@@ -130,6 +132,7 @@ function MakeScriptO2 {
   O2ARGS_TASK_JPSI="$O2ARGS"
   O2ARGS_TASK_DPLUS="$O2ARGS"
   O2ARGS_TASK_LC="$O2ARGS"
+  O2ARGS_TASK_BPLUS="$O2ARGS"
   # MC
   [ "$ISMC" -eq 1 ] && {
     O2ARGS_CAND_2PRONG+=" --doMC"
@@ -153,6 +156,7 @@ function MakeScriptO2 {
   O2EXEC_TASK_JPSI="o2-analysis-hf-task-jpsi $O2ARGS_TASK_JPSI"
   O2EXEC_TASK_DPLUS="o2-analysis-hf-task-dplus $O2ARGS_TASK_DPLUS"
   O2EXEC_TASK_LC="o2-analysis-hf-task-lc $O2ARGS_TASK_LC"
+  O2EXEC_TASK_BPLUS="o2-analysis-hf-task-bplus $O2ARGS_TASK_BPLUS"
 
   # Form the full O2 command.
   [[ $DOO2_QA -eq 1 && $ISMC -eq 0 ]] && { MsgWarn "Skipping the QA task for non-MC input"; DOO2_QA=0; } # Disable running the QA task for non-MC input.
@@ -171,6 +175,7 @@ function MakeScriptO2 {
   [ $DOO2_TASK_DPLUS -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_DPLUS"; MsgSubStep "  hf-task-dplus"; }
   [ $DOO2_TASK_LC -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_LC"; MsgSubStep "  hf-task-lc"; }
   [ $DOO2_TASK_JPSI -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_JPSI"; MsgSubStep "  hf-task-jpsi"; }
+  [ $DOO2_TASK_BPLUS -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_BPLUS"; MsgSubStep "  hf-task-bplus"; }
   O2EXEC=${O2EXEC:3} # Remove the leading " | ".
   [ "$O2EXEC" ] || ErrExit "Nothing to do!"
 
