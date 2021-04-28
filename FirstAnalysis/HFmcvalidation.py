4ee7372b49a40e8bfa3db8840d12d2771418f0ce
marianna.mazzilli@cern.ch <mmazzill@aliceml.cern.ch>
2021-04-28 21:58:16 +0200

    Adding utilities for performance plots

--- FirstAnalysis/HFmcvalidation.py
+++ FirstAnalysis/HFmcvalidation.py
@@ -1,4 +1,5 @@
-from ROOT import TCanvas, TFile, TGaxis, TLegend, gStyle
+#!/usr/bin/env python
+from ROOT import TCanvas, TFile, TGaxis, TLatex, TLegend, gStyle
 
 
 def printCanvas(canvas, title):
@@ -15,7 +16,7 @@ def count_HFquarks_perColl(file, var):
     hq.GetXaxis().SetRangeUser(0.0, 8.0)
     hq.SetTitle("Number of %s quarks per collision" % var)
     hq.Draw("")
-    printCanvas(cquark, "QuarkCountsPerCollision%s" % var)
+    printCanvas(cquark, "BtoDForcedProduction/QuarkCountsPerCollision%s" % var)
 
 
 def count_Particle_perColl(file, var):
@@ -24,7 +25,7 @@ def count_Particle_perColl(file, var):
     cparticle.Divide(2, 1)
     hp = file.Get("hf-mc-validation-gen/hCouterPerCollision%s" % var)
     hp.Draw("")
-    printCanvas(cparticle, "%sPerCollision" % var)
+    printCanvas(cparticle, "BtoDForcedProduction/%sPerCollision" % var)
 
 
 def momentum_Conservation(file, var):
@@ -38,7 +39,7 @@ def momentum_Conservation(file, var):
         hp = file.Get("hf-mc-validation-gen/h%sDiffMotherDaughterGen" % var)
         hp.SetTitle("Momentum Conservation: %s component" % var)
     hp.Draw("")
-    printCanvas(cmomentum, "%sconservation" % var)
+    printCanvas(cmomentum, "BtoDForcedProduction/%sconservation" % var)
 
 
 def momentum_check(file):
@@ -46,7 +47,7 @@ def momentum_check(file):
     cMomCheck.SetCanvasSize(900, 700)
     hMomCheck = file.Get("hf-mc-validation-gen/hMomentumCheck")
     hMomCheck.Draw()
-    printCanvas(cMomCheck, "MomentumCheck")
+    printCanvas(cMomCheck, "BtoDForcedProduction/MomentumCheck")
 
 
 def p_diff_reco_MC(file):
@@ -54,7 +55,7 @@ def p_diff_reco_MC(file):
     components_list = ["x", "y", "z"]
     color_list = [1, 2, 4]
     cpt = TCanvas("c1", "Momentum Difference")
-    cpt.SetCanvasSize(1500, 1500)
+    cpt.SetCanvasSize(1000, 700)
     cpt.cd()
     mom_list = []
     leg = TLegend(0.1, 0.7, 0.4, 0.9, "")
@@ -74,35 +75,80 @@ def p_diff_reco_MC(file):
             hp.Draw()
         leg.AddEntry(mom_list[i], "P%s component" % comp)
     leg.Draw()
-    printCanvas(cpt, "p_recoMC_diff")
+    printCanvas(cpt, "BtoDForcedProduction/p_recoMC_diff")
 
 
 def secondary_vertex_reco_MC(file):
     gStyle.SetOptStat(0)
+    gStyle.SetOptTitle(0)
     TGaxis.SetMaxDigits(3)
     components_list = ["x", "y", "z"]
     color_list = [1, 2, 4]
+    line_style_list = [6, 2, 3]
     csecvertex = TCanvas("c1", "Secondary Vertex Difference")
     csecvertex.SetCanvasSize(1000, 700)
+    csecvertex.SetLogy()
     csecvertex.cd()
     secvertex_list = []
-    leg = TLegend(0.1, 0.7, 0.4, 0.9, "Sec. Vertex:")
+    leg = TLegend(0.7, 0.7, 0.9, 0.9)
+    leg.SetTextSize(0.04)
     leg.SetFillColor(0)
     for i, comp in enumerate(components_list):
         hsecvertex = file.Get("hf-mc-validation-rec/histSecV%s" % comp)
-        hsecvertex.Rebin(1)
+        hsecvertex.Rebin(2)
         hsecvertex.SetLineColor(color_list[i])
-        hsecvertex.SetLineWidth(2)
+        hsecvertex.SetLineWidth(3)
+        hsecvertex.SetLineStyle(line_style_list[i])
+        hsecvertex.GetXaxis().SetRangeUser(-0.01, 0.01)
+        hsecvertex.GetYaxis().SetRangeUser(0.1, 1200)
         secvertex_list.append(hsecvertex)
         if i != 0:
             hsecvertex.Draw("same")
+            hsecvertex.Fit("gaus", "", "", -0.001, 0.001)
         else:
-            hsecvertex.SetTitle("Secondary Vertex difference Rec-Gen")
-            hsecvertex.GetXaxis().SetTitle("V_{i}^{reco}-V_{i}^{gen} (cm)")
-            hsecvertex.Draw()
-        leg.AddEntry(secvertex_list[i], "%s component" % comp)
-    leg.Draw()
-    printCanvas(csecvertex, "SecVertex_recoMC_diff")
+            hsecvertex.GetYaxis().SetTitle("Counts")
+            hsecvertex.GetYaxis().SetTitleSize(0.05)
+            hsecvertex.GetYaxis().SetTitleOffset(0.8)
+            hsecvertex.GetXaxis().SetTitle("vtx_{reco}^{sec}-vtx_{gen}^{sec} (cm)")
+            hsecvertex.GetXaxis().SetTitleSize(0.05)
+            hsecvertex.GetXaxis().SetTitleOffset(0.8)
+            # hsecvertex.Fit("gaus")
+            hsecvertex.Draw("")
+        leg.AddEntry(
+            secvertex_list[i],
+            "#it{%s}_{reco}^{sec} - #it{%s}_{gen}^{sec}" % (comp, comp),
+        )
+    leg.Draw("p")
+    latex = TLatex()
+    latex0 = TLatex()
+    latex1 = TLatex()
+    latex2 = TLatex()
+    latex3 = TLatex()
+    latex4 = TLatex()
+    latex.SetTextAlign(12)
+    latex.SetTextFont(62)
+    latex.DrawLatex(-0.0085, 600, "ALICE 3 Projections")
+    latex1.SetTextAlign(12)
+    latex1.SetTextFont(42)
+    latex1.SetTextSize(0.045)
+    latex1.DrawLatex(-0.0085, 350, "PYTHIA 8.2")
+    latex0.SetTextAlign(12)
+    latex0.SetTextSize(0.045)
+    latex0.SetTextFont(42)
+    latex0.DrawLatex(-0.0085, 200, "pp #sqrt{#it{s}} = 14 TeV")
+    latex2.SetTextAlign(12)
+    latex2.SetTextSize(0.045)
+    latex2.SetTextFont(42)
+    latex2.DrawLatex(-0.0085, 100, "#it{R}_{innermost layer} = 5mm")
+    latex3.SetTextAlign(12)
+    latex3.SetTextSize(0.045)
+    latex3.SetTextFont(42)
+    latex3.DrawLatex(0.0032, 55, "#Lambda_{c}#rightarrow pK#pi, |#eta|< 2")
+    latex4.SetTextAlign(12)
+    latex4.SetTextSize(0.045)
+    latex4.SetTextFont(42)
+    latex4.DrawLatex(0.0032, 20, "4 < #it{p}_{T}(#Lambda_{c})< 6 GeV/#it{c}")
+    printCanvas(csecvertex, "BtoDForcedProduction/SecVertex_recoMC_diff_last_4_6")
 
 
 def decayLength_reco_MC(file):
@@ -111,10 +157,13 @@ def decayLength_reco_MC(file):
     hDecLenRes = file.Get("hf-mc-validation-rec/histDecLen")
     hDecLenRes.Rebin(2)
     hDecLenRes.Draw()
-    printCanvas(cDecLenRes, "Resolution_DecayLength")
+    printCanvas(cDecLenRes, "BtoDForcedProduction/Resolution_DecayLength")
 
 
-fileo2 = TFile("../codeHF/CCbar_pp_AnalysisResults_O2_500files.root")
+# fileo2 = TFile("/data/Run5/plots/new_pro/AnalysisResults_O2_pp14TeV_ccbar_10M_sc3werner_20042021.root")
+fileo2 = TFile(
+    "/home/mmazzill/alice/Run3Analysisvalidation/codeHF/AnalysisResults_O2_LcpKpi_4_6.root"
+)
 # Generated Level Validation
 quark_list = ["C", "Cbar", "B", "Bbar"]
 for quark in quark_list:
--- FirstAnalysis/trackingstudies.py
+++ FirstAnalysis/trackingstudies.py
@@ -3,14 +3,13 @@ from ROOT import TCanvas, TFile, TLegend, gStyle
 
 
 def saveCanvas(canvas, title):
-    format_list = ["png", ".pdf", ".root"]
+    format_list = [".png", ".pdf", ".root"]
     for fileFormat in format_list:
         canvas.SaveAs(title + fileFormat)
 
 
 # superimposed reco and gen distributions per specie
-def specie_distribution(had, var):
-    fileo2 = TFile("../codeHF/AnalysisResults_O2.root")
+def specie_distribution(fileo2, had, var):
     gStyle.SetOptStat(0)
     cpt = TCanvas("cpt", "pT distribution")
     cpt.SetCanvasSize(1500, 700)
@@ -32,11 +31,10 @@ def specie_distribution(had, var):
     leg.AddEntry(hnum, "Rec. Level")
     leg.AddEntry(hden, "Gen. Level")
     leg.Draw()
-    saveCanvas(cpt, "%sdistribution_hfcand%s" % (var, had))
+    saveCanvas(cpt, "%sdistribution_hfcand%s_pp_werner" % (var, had))
 
 
-def multiplicity():
-    fileo2 = TFile("../codeHF/AnalysisResults_O2.root")
+def multiplicity(fileo2):
     cmult = TCanvas("cmult", "mult distribution")
     cmult.SetCanvasSize(1500, 700)
     cmult.SetLogy()
@@ -45,59 +43,60 @@ def multiplicity():
     hmult.Rebin(4)
     hmult.Draw()
     hmult.SetTitle("Multiplicity Distribution")
-    saveCanvas(cmult, "multiplicity")
+    saveCanvas(cmult, "multiplicity_pp_werner")
 
 
-def var_tracking(var):
-    fileo2 = TFile("../codeHF/AnalysisResults_O2.root")
+def var_tracking(fileo2, var):
     cvar = TCanvas("cvar", "var distribution")
     cvar.SetCanvasSize(1800, 800)
     cvar.Divide(2, 1)
     hvar = fileo2.Get("qa-tracking-kine/tracking/%s" % var)
     hvar.Rebin(4)
     hvar.Draw()
-    saveCanvas(cvar, "tracking_%s" % var)
+    saveCanvas(cvar, "tracking_%s_KrKr_werner" % var)
 
 
-def tracking_resolution(var1, mode, var2, var3):
-    fileo2 = TFile("../codeHF/AnalysisResults_O2.root")
+def tracking_resolution(fileo2, var1, mode, var2, var3):
     cres = TCanvas("cres", "resolution distribution")
     cres.SetCanvasSize(1500, 700)
     if var1 == "eta":
         heta = fileo2.Get("qa-tracking-resolution/eta/etaDiffMCRecoVsEta%s" % mode)
         heta.Draw("coltz")
         heta.SetTitle("%s resolution" % var1)
-        saveCanvas(cres, "Resolution_%s_%s" % (var1, var2))
+        saveCanvas(cres, "Resolution_%s_%sKrKR_werner" % (var1, var2))
 
     if var1 == "impactParameter":
         himp_par = fileo2.Get(
             "qa-tracking-resolution/%s/%s%sVs%s" % (var1, var1, var2, var3)
         )
+        cres.SetLogz()
         himp_par.Draw("coltz")
         himp_par.SetTitle("Impact Parameter resolution: %s Vs %s" % (var2, var3))
-        saveCanvas(cres, "impact_parameter_resolution%sVs%s" % (var2, var3))
+        saveCanvas(cres, "impact_parameter_resolution%sVs%s_KrKr_novtx" % (var2, var3))
     if var1 == "impactParameterError":
         himp_par_err = fileo2.Get(
             "qa-tracking-resolution/impactParameter/%s%sVs%s" % (var1, var2, var3)
         )
+        cres.SetLogz()
         himp_par_err.Draw("coltz")
         himp_par_err.SetTitle(
             "Impact Parameter Error resolution: %s Vs %s" % (var2, var3)
         )
-        saveCanvas(cres, "impactParameterError_resolution%sVs%s" % (var2, var3))
+        saveCanvas(
+            cres, "impactParameterError_resolution%sVs%s_KrKr_novtx" % (var2, var3)
+        )
     if var1 == "phi":
         hphi = fileo2.Get("qa-tracking-resolution/phi/phiDiffMCRec")
         hphi.Draw()
-        saveCanvas(cres, "phi_resolution")
+        saveCanvas(cres, "phi_resolution_KrKr_new")
 
     if var1 == "pt":
         h_pt = fileo2.Get("qa-tracking-resolution/pt/ptResolutionVs%s" % var2)
         h_pt.Draw("coltz")
-        saveCanvas(cres, "ptResolutionVs%s" % var2)
+        saveCanvas(cres, "ptResolutionVs%s_KrKr_new" % var2)
 
 
-def vertex_distributions(var):
-    fileo2 = TFile("../codeHF/AnalysisResults_O2.root")
+def vertex_distributions(fileo2, var):
     cvertex = TCanvas("cvertex", "vertex ")
     cvertex.SetCanvasSize(1800, 700)
     cvertex.Divide(2, 1)
@@ -106,29 +105,35 @@ def vertex_distributions(var):
     hvertex.Draw()
     hvertex_res = fileo2.Get("qa-global-observables/collision/%svsNContrib" % var)
     cvertex.cd(2)
+    cvertex.cd(2).SetLogz()
     hvertex_res.Draw("coltz")
-    saveCanvas(cvertex, "vertex_%s" % var)
+    saveCanvas(cvertex, "vertex_%s_KrKr_new_novtx" % var)
 
 
-multiplicity()
+# fileo2 = TFile("/data/Run5/plots/new_0427/KrKr_6p460TeV_inel_200K_sc3_werner_22042021_-2_2.root")
+fileo2 = TFile(
+    "/home/mmazzill/alice/Run3Analysisvalidation/codeHF/AnalysisResults_O2_KrKr_NOVtx.root"
+)
+# multiplicity(fileo2)
 specie_list = ["pion", "proton", "kaon", "electron", "muon"]
 var_list = ["pt", "eta", "phi"]
 for specie in specie_list:
     for var in var_list:
-        specie_distribution(specie, var)
+        specie_distribution(fileo2, specie, var)
 
 vertex_coord_list = ["X", "Y", "Z"]
 for coord in vertex_coord_list:
-    vertex_distributions(coord)
+    vertex_distributions(fileo2, coord)
 
 for var in var_list:
-    var_tracking(var)
+    var_tracking(fileo2, var)
 
 var1_list = ["RPhi", "Z"]
 var2_list = ["Pt", "Phi", "Eta"]
-tracking_resolution("eta", "MC", "", "")
-tracking_resolution("pt", "", "Eta", "")
+tracking_resolution(fileo2, "eta", "MC", "", "")
+tracking_resolution(fileo2, "pt", "", "Eta", "")
+tracking_resolution(fileo2, "pt", "", "Pt", "")
 for var1 in var1_list:
     for var2 in var2_list:
-        tracking_resolution("impactParameter", "MC", var1, var2)
-        tracking_resolution("impactParameterError", "MC", var1, var2)
+        tracking_resolution(fileo2, "impactParameter", "MC", var1, var2)
+        tracking_resolution(fileo2, "impactParameterError", "MC", var1, var2)
