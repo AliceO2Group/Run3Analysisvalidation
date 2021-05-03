88a6561f12a10c07a73d26edb9f6927df6500608
Vít Kučera <vit.kucera@cern.ch>
2021-05-04 00:10:54 +0200

    Reset activation flags.

--- codeHF/config_tasks.sh
+++ codeHF/config_tasks.sh
@@ -31,7 +31,7 @@ MAKE_GRAPH=0        # Make topology graph.
 # QA
 DOO2_QA_EFF=0       # qa-efficiency
 DOO2_QA_SIM=0       # qa-simple
-DOO2_MC_VALID=1     # hf-mc-validation
+DOO2_MC_VALID=0     # hf-mc-validation
 # PID
 DOO2_PID_TPC=0      # pid-tpc
 DOO2_PID_TOF=0      # pid-tof
@@ -48,15 +48,15 @@ DOO2_SEL_XIC=0      # hf-xic-topkpi-candidate-selector
 DOO2_SEL_JPSI=0     # hf-jpsi-toee-candidate-selector
 # User tasks
 DOO2_TASK_D0=1      # hf-task-d0
-DOO2_TASK_DPLUS=1   # hf-task-dplus
-DOO2_TASK_LC=1      # hf-task-lc
-DOO2_TASK_XIC=1     # hf-task-xic
+DOO2_TASK_DPLUS=0   # hf-task-dplus
+DOO2_TASK_LC=0      # hf-task-lc
+DOO2_TASK_XIC=0     # hf-task-xic
 DOO2_TASK_JPSI=0    # hf-task-jpsi
-DOO2_TASK_BPLUS=1   # hf-task-bplus
+DOO2_TASK_BPLUS=0   # hf-task-bplus
 DOO2_TASK_X=0       # hf-task-x
 # Tree creators
-DOO2_TREE_D0=1      # hf-tree-creator-d0-tokpi
-DOO2_TREE_LC=1      # hf-tree-creator-lc-topkpi
+DOO2_TREE_D0=0      # hf-tree-creator-d0-tokpi
+DOO2_TREE_LC=0      # hf-tree-creator-lc-topkpi
 
 # Selection cuts
 APPLYCUTS_D0=0      # Apply D0 selection cuts.
