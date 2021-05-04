c5c583ca7f744047789771318cb1707372d74fc1
Vít Kučera <vit.kucera@cern.ch>
2021-05-04 15:00:01 +0200

    Introduce a utility to automatically generate O2 command from a YAML database. (#208)

--- codeHF/config_tasks.sh
+++ codeHF/config_tasks.sh
@@ -23,15 +23,19 @@ DOPOSTPROCESS=1     # Run output postprocessing. (Compare AliPhysics and O2 outp
 # Disable incompatible steps.
 [ "$ISINPUTO2" -eq 1 ] && { DOCONVERT=0; DOALI=0; }
 
-# Activation of O2 tasks
+# O2 database
+DATABASE_O2="workflows.yml"
+MAKE_GRAPH=0        # Make topology graph.
+
+# Activation of O2 workflows
 # QA
-DOO2_QA_EFF=1       # qa-efficiency
-DOO2_QA_SIM=1       # qa-simple
+DOO2_QA_EFF=0       # qa-efficiency
+DOO2_QA_SIM=0       # qa-simple
 DOO2_MC_VALID=0     # hf-mc-validation
 # PID
 DOO2_PID_TPC=0      # pid-tpc
 DOO2_PID_TOF=0      # pid-tof
-DOO2_PID_TOF_QA=0   # pid-tof-qa
+DOO2_PID_TOF_QA=0   # pid-tof-qa-mc
 # Vertexing
 DOO2_SKIM=0         # hf-track-index-skims-creator
 DOO2_CAND_2PRONG=0  # hf-candidate-creator-2prong
@@ -125,142 +129,42 @@ function AdjustJson {
 
 # Generate the O2 script containing the full workflow specification.
 function MakeScriptO2 {
-  # Deactivate MC-only tasks for non-MC input.
-  [ "$ISMC" -eq 0 ] && {
-    [ $DOO2_QA_EFF -eq 1 ] && { MsgWarn "Skipping the QA task efficiency for non-MC input"; DOO2_QA_EFF=0; }
-    [ $DOO2_QA_SIM -eq 1 ] && { MsgWarn "Skipping the QA task simple for non-MC input"; DOO2_QA_SIM=0; }
-    [ $DOO2_PID_TOF_QA -eq 1 ] && { MsgWarn "Skipping the TOF PID QA for non-MC input"; DOO2_PID_TOF_QA=0; }
-    [ $DOO2_TREE_D0 -eq 1 ] && { MsgWarn "Skipping the D0 tree creator for non-MC input"; DOO2_TREE_D0=0; }
-    [ $DOO2_TREE_LC -eq 1 ] && { MsgWarn "Skipping the Λc tree creator for non-MC input"; DOO2_TREE_LC=0; }
-    [ $DOO2_MC_VALID -eq 1 ] && { MsgWarn "Skipping the MC validation task for non-MC input"; DOO2_MC_VALID=0; }
-  }
+  WORKFLOWS=""
+  [ $DOO2_QA_EFF -eq 1 ] && WORKFLOWS+=" o2-analysis-qa-efficiency"
+  [ $DOO2_QA_SIM -eq 1 ] && WORKFLOWS+=" o2-analysis-qa-simple"
+  [ $DOO2_SKIM -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-track-index-skims-creator"
+  [ $DOO2_CAND_2PRONG -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-candidate-creator-2prong"
+  [ $DOO2_CAND_3PRONG -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-candidate-creator-3prong"
+  [ $DOO2_PID_TPC -eq 1 ] && WORKFLOWS+=" o2-analysis-pid-tpc"
+  [ $DOO2_PID_TOF -eq 1 ] && WORKFLOWS+=" o2-analysis-pid-tof"
+  [ $DOO2_PID_TOF_QA -eq 1 ] && WORKFLOWS+=" o2-analysis-pid-tof-qa-mc"
+  [ $DOO2_SEL_D0 -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-d0-candidate-selector"
+  [ $DOO2_SEL_JPSI -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-jpsi-toee-candidate-selector"
+  [ $DOO2_SEL_DPLUS -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-dplus-topikpi-candidate-selector"
+  [ $DOO2_SEL_LC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-lc-candidate-selector"
+  [ $DOO2_SEL_XIC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-xic-topkpi-candidate-selector"
+  [ $DOO2_TASK_D0 -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-d0"
+  [ $DOO2_TASK_JPSI -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-jpsi"
+  [ $DOO2_TASK_DPLUS -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-dplus"
+  [ $DOO2_TASK_LC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-lc"
+  [ $DOO2_TASK_XIC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-xic"
+  [ $DOO2_TASK_BPLUS -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-bplus"
+  [ $DOO2_TASK_X -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-task-x"
+  [ $DOO2_TREE_D0 -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-tree-creator-d0-tokpi"
+  [ $DOO2_TREE_LC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-tree-creator-lc-topkpi"
+  [ $DOO2_MC_VALID -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-mc-validation"
 
-  # Handle dependencies. (latest first)
-  # QA
-  [ $DOO2_MC_VALID -eq 1 ] && { DOO2_SEL_D0=1; }
-  # Tree creators
-  [ $DOO2_TREE_D0 -eq 1 ] && { DOO2_SEL_D0=1; }
-  [ $DOO2_TREE_LC -eq 1 ] && { DOO2_SEL_LC=1; }
-  # User tasks
-  [ $DOO2_TASK_D0 -eq 1 ] && { DOO2_SEL_D0=1; }
-  [ $DOO2_TASK_JPSI -eq 1 ] && { DOO2_SEL_JPSI=1; }
-  [ $DOO2_TASK_LC -eq 1 ] && { DOO2_SEL_LC=1; }
-  [ $DOO2_TASK_XIC -eq 1 ] && { DOO2_SEL_XIC=1; }
-  [ $DOO2_TASK_DPLUS -eq 1 ] && { DOO2_SEL_DPLUS=1; }
-  [ $DOO2_TASK_BPLUS -eq 1 ] && { DOO2_SEL_D0=1; }
-  [ $DOO2_TASK_X -eq 1 ] && { DOO2_SEL_JPSI=1; }
-  # Selectors
-  [[ $DOO2_SEL_D0 -eq 1 || $DOO2_SEL_JPSI -eq 1 ]] && { DOO2_CAND_2PRONG=1; DOO2_PID_TPC=1; DOO2_PID_TOF=1; }
-  [[ $DOO2_SEL_LC -eq 1 || $DOO2_SEL_XIC -eq 1 || $DOO2_SEL_DPLUS -eq 1 ]] && { DOO2_CAND_3PRONG=1; DOO2_PID_TPC=1; DOO2_PID_TOF=1; }
-  # Vertexing
-  [[ $DOO2_CAND_2PRONG -eq 1 || $DOO2_CAND_3PRONG -eq 1 ]] && { DOO2_SKIM=1; }
-  # PID
-  [ $DOO2_PID_TOF_QA -eq 1 ] && { DOO2_PID_TOF=1; }
+  # Translate options into arguments of the generating script.
+  OPT_MAKECMD=""
+  [ "$ISMC" -eq 1 ] && OPT_MAKECMD+=" --mc"
+  [ "$DEBUG" -eq 1 ] && OPT_MAKECMD+=" -d"
+  [ $SAVETREES -eq 1 ] && OPT_MAKECMD+=" -t"
+  [ $MAKE_GRAPH -eq 1 ] && OPT_MAKECMD+=" -g"
 
-  # General options
-  O2ARGS="--aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --configuration json://\$JSON -b"
-
-  # Options to save tables in trees
-  [ $SAVETREES -eq 1 ] && {
-    MsgWarn "Tables will be saved in trees."
-    O2TABLES=""
-    [ $DOO2_SKIM -eq 1 ] && { O2TABLES+=",AOD/HFSELTRACK/0,AOD/HFTRACKIDXP2/0,AOD/HFTRACKIDXP3/0"; }
-    [ $DOO2_CAND_2PRONG -eq 1 ] && { O2TABLES+=",AOD/HFCANDP2BASE/0,AOD/HFCANDP2EXT/0"; [ "$ISMC" -eq 1 ] && O2TABLES+=",AOD/HFCANDP2MCREC/0,AOD/HFCANDP2MCGEN/0"; }
-    [ $DOO2_CAND_3PRONG -eq 1 ] && { O2TABLES+=",AOD/HFCANDP3BASE/0,AOD/HFCANDP3EXT/0"; [ "$ISMC" -eq 1 ] && O2TABLES+=",AOD/HFCANDP3MCREC/0,AOD/HFCANDP3MCGEN/0"; }
-    [ $DOO2_TREE_D0 -eq 1 ] && { O2TABLES+=",AOD/HFCANDP2Full/0,AOD/HFCANDP2FullE/0,AOD/HFCANDP2FullP/0"; }
-    [ $DOO2_TREE_LC -eq 1 ] && { O2TABLES+=",AOD/HFCANDP3Full/0,AOD/HFCANDP3FullE/0,AOD/HFCANDP3FullP/0"; }
-    O2TABLES=${O2TABLES:1} # Remove the leading comma.
-    # shellcheck disable=SC2015 # Ignore A && B || C.
-    [ "$O2TABLES" ] && { O2ARGS+=" --aod-writer-keep $O2TABLES"; } || { ErrExit "Empty list of tables!"; }
-  }
-
-  # Task-specific options
-  O2ARGS_QA_EFF="$O2ARGS --eff-el 1 --eff-mu 1 --eff-pi 1 --eff-ka 1 --eff-pr 1"
-  O2ARGS_QA_SIM="$O2ARGS"
-  O2ARGS_SKIM="$O2ARGS"
-  O2ARGS_CAND_2PRONG="$O2ARGS"
-  O2ARGS_CAND_3PRONG="$O2ARGS"
-  O2ARGS_PID_TPC="$O2ARGS --pid-el 1 --pid-mu 1 --pid-pi 1 --pid-ka 1 --pid-pr 1"
-  O2ARGS_PID_TOF="$O2ARGS --pid-el 1 --pid-mu 1 --pid-pi 1 --pid-ka 1 --pid-pr 1"
-  O2ARGS_PID_TOF_QA="$O2ARGS"
-  O2ARGS_SEL_D0="$O2ARGS"
-  O2ARGS_SEL_JPSI="$O2ARGS"
-  O2ARGS_SEL_DPLUS="$O2ARGS"
-  O2ARGS_SEL_LC="$O2ARGS"
-  O2ARGS_SEL_XIC="$O2ARGS"
-  O2ARGS_TASK_D0="$O2ARGS"
-  O2ARGS_TASK_JPSI="$O2ARGS"
-  O2ARGS_TASK_DPLUS="$O2ARGS"
-  O2ARGS_TASK_LC="$O2ARGS"
-  O2ARGS_TASK_XIC="$O2ARGS"
-  O2ARGS_TASK_BPLUS="$O2ARGS"
-  O2ARGS_TASK_X="$O2ARGS"
-  O2ARGS_TREE_D0="$O2ARGS"
-  O2ARGS_TREE_LC="$O2ARGS"
-  O2ARGS_MC_VALID="$O2ARGS"
-  # MC
-  [ "$ISMC" -eq 1 ] && {
-    O2ARGS_CAND_2PRONG+=" --doMC"
-    O2ARGS_CAND_3PRONG+=" --doMC"
-    O2ARGS_TASK_D0+=" --doMC"
-    O2ARGS_TASK_DPLUS+=" --doMC"
-    O2ARGS_TASK_LC+=" --doMC"
-    O2ARGS_TASK_XIC+=" --doMC"
-    O2ARGS_TASK_JPSI+=" --doMC"
-  }
-
-  # Pair O2 executables with their respective options.
-  O2EXEC_QA_EFF="o2-analysis-qa-efficiency $O2ARGS_QA_EFF"
-  O2EXEC_QA_SIM="o2-analysis-qa-simple $O2ARGS_QA_SIM"
-  O2EXEC_SKIM="o2-analysis-hf-track-index-skims-creator $O2ARGS_SKIM"
-  O2EXEC_CAND_2PRONG="o2-analysis-hf-candidate-creator-2prong $O2ARGS_CAND_2PRONG"
-  O2EXEC_CAND_3PRONG="o2-analysis-hf-candidate-creator-3prong $O2ARGS_CAND_3PRONG"
-  O2EXEC_PID_TPC="o2-analysis-pid-tpc $O2ARGS_PID_TPC"
-  O2EXEC_PID_TOF="o2-analysis-pid-tof $O2ARGS_PID_TOF"
-  O2EXEC_PID_TOF_QA="o2-analysis-pid-tof-qa $O2ARGS_PID_TOF_QA"
-  O2EXEC_SEL_D0="o2-analysis-hf-d0-candidate-selector $O2ARGS_SEL_D0"
-  O2EXEC_SEL_DPLUS="o2-analysis-hf-dplus-topikpi-candidate-selector $O2ARGS_SEL_DPLUS"
-  O2EXEC_SEL_LC="o2-analysis-hf-lc-candidate-selector $O2ARGS_SEL_LC"
-  O2EXEC_SEL_XIC="o2-analysis-hf-xic-topkpi-candidate-selector $O2ARGS_SEL_XIC"
-  O2EXEC_SEL_JPSI="o2-analysis-hf-jpsi-toee-candidate-selector $O2ARGS_SEL_JPSI"
-  O2EXEC_TASK_D0="o2-analysis-hf-task-d0 $O2ARGS_TASK_D0"
-  O2EXEC_TASK_JPSI="o2-analysis-hf-task-jpsi $O2ARGS_TASK_JPSI"
-  O2EXEC_TASK_DPLUS="o2-analysis-hf-task-dplus $O2ARGS_TASK_DPLUS"
-  O2EXEC_TASK_LC="o2-analysis-hf-task-lc $O2ARGS_TASK_LC"
-  O2EXEC_TASK_XIC="o2-analysis-hf-task-xic $O2ARGS_TASK_XIC"
-  O2EXEC_TASK_BPLUS="o2-analysis-hf-task-bplus $O2ARGS_TASK_BPLUS"
-  O2EXEC_TASK_X="o2-analysis-hf-task-x $O2ARGS_TASK_X"
-  O2EXEC_TREE_D0="o2-analysis-hf-tree-creator-d0-tokpi $O2ARGS_TREE_D0"
-  O2EXEC_TREE_LC="o2-analysis-hf-tree-creator-lc-topkpi $O2ARGS_TREE_LC"
-  O2EXEC_MC_VALID="o2-analysis-hf-mc-validation $O2ARGS_MC_VALID"
-
-  # Form the full O2 command.
-  echo "Tasks to be executed:"
-  O2EXEC=""
-  [ $DOO2_QA_EFF -eq 1 ] && { O2EXEC+=" | $O2EXEC_QA_EFF"; MsgSubStep "  qa-efficiency"; }
-  [ $DOO2_QA_SIM -eq 1 ] && { O2EXEC+=" | $O2EXEC_QA_SIM"; MsgSubStep "  qa-simple"; }
-  [ $DOO2_SKIM -eq 1 ] && { O2EXEC+=" | $O2EXEC_SKIM"; MsgSubStep "  hf-track-index-skims-creator"; }
-  [ $DOO2_CAND_2PRONG -eq 1 ] && { O2EXEC+=" | $O2EXEC_CAND_2PRONG"; MsgSubStep "  hf-candidate-creator-2prong"; }
-  [ $DOO2_CAND_3PRONG -eq 1 ] && { O2EXEC+=" | $O2EXEC_CAND_3PRONG"; MsgSubStep "  hf-candidate-creator-3prong"; }
-  [ $DOO2_PID_TPC -eq 1 ] && { O2EXEC+=" | $O2EXEC_PID_TPC"; MsgSubStep "  pid-tpc"; }
-  [ $DOO2_PID_TOF -eq 1 ] && { O2EXEC+=" | $O2EXEC_PID_TOF"; MsgSubStep "  pid-tof"; }
-  [ $DOO2_PID_TOF_QA -eq 1 ] && { O2EXEC+=" | $O2EXEC_PID_TOF_QA"; MsgSubStep "  pid-tof-qa"; }
-  [ $DOO2_SEL_D0 -eq 1 ] && { O2EXEC+=" | $O2EXEC_SEL_D0"; MsgSubStep "  hf-d0-candidate-selector"; }
-  [ $DOO2_SEL_JPSI -eq 1 ] && { O2EXEC+=" | $O2EXEC_SEL_JPSI"; MsgSubStep "  hf-jpsi-toee-candidate-selector"; }
-  [ $DOO2_SEL_DPLUS -eq 1 ] && { O2EXEC+=" | $O2EXEC_SEL_DPLUS"; MsgSubStep "  hf-dplus-topikpi-candidate-selector"; }
-  [ $DOO2_SEL_LC -eq 1 ] && { O2EXEC+=" | $O2EXEC_SEL_LC"; MsgSubStep "  hf-lc-candidate-selector"; }
-  [ $DOO2_SEL_XIC -eq 1 ] && { O2EXEC+=" | $O2EXEC_SEL_XIC"; MsgSubStep "  hf-xic-topkpi-candidate-selector"; }
-  [ $DOO2_TASK_D0 -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_D0"; MsgSubStep "  hf-task-d0"; }
-  [ $DOO2_TASK_DPLUS -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_DPLUS"; MsgSubStep "  hf-task-dplus"; }
-  [ $DOO2_TASK_LC -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_LC"; MsgSubStep "  hf-task-lc"; }
-  [ $DOO2_TASK_XIC -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_XIC"; MsgSubStep "  hf-task-xic"; }
-  [ $DOO2_TASK_JPSI -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_JPSI"; MsgSubStep "  hf-task-jpsi"; }
-  [ $DOO2_TASK_BPLUS -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_BPLUS"; MsgSubStep "  hf-task-bplus"; }
-  [ $DOO2_TASK_X -eq 1 ] && { O2EXEC+=" | $O2EXEC_TASK_X"; MsgSubStep "  hf-task-x"; }
-  [ $DOO2_TREE_D0 -eq 1 ] && { O2EXEC+=" | $O2EXEC_TREE_D0"; MsgSubStep "  hf-tree-creator-d0-tokpi"; }
-  [ $DOO2_TREE_LC -eq 1 ] && { O2EXEC+=" | $O2EXEC_TREE_LC"; MsgSubStep "  hf-tree-creator-lc-topkpi"; }
-  [ $DOO2_MC_VALID -eq 1 ] && { O2EXEC+=" | $O2EXEC_MC_VALID"; MsgSubStep "  hf-mc-validation"; }
-  O2EXEC=${O2EXEC:3} # Remove the leading " | ".
+  # Generate the O2 command.
+  MAKECMD="python3 $DIR_EXEC/make_command_o2.py $DATABASE_O2 $OPT_MAKECMD"
+  O2EXEC=$($MAKECMD -w "$WORKFLOWS")
+  $MAKECMD -w "$WORKFLOWS" 1> /dev/null 2> /dev/null || ErrExit "Generating of O2 command failed."
   [ "$O2EXEC" ] || ErrExit "Nothing to do!"
 
   # Create the script with the full O2 command.
@@ -269,7 +173,7 @@ function MakeScriptO2 {
 FileIn="\$1"
 JSON="\$2"
 mkdir sockets && \
-$O2EXEC --fairmq-ipc-prefix sockets && \
+$O2EXEC && \
 rm -r sockets
 EOF
 }
