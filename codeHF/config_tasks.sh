20767727aca728e6f996e90dbe4f6dd0d32b1efd
Vít Kučera <vit.kucera@cern.ch>
2021-05-05 13:05:15 +0200

    Fix defaults

--- codeHF/config_tasks.sh
+++ codeHF/config_tasks.sh
@@ -52,7 +52,7 @@ DOO2_SEL_XIC=0      # hf-xic-topkpi-candidate-selector
 DOO2_SEL_JPSI=0     # hf-jpsi-toee-candidate-selector
 DOO2_SEL_LCK0SP=0   # hf-lc-tok0sp-candidate-selector
 # User tasks
-DOO2_TASK_D0=0      # hf-task-d0
+DOO2_TASK_D0=1      # hf-task-d0
 DOO2_TASK_DPLUS=0   # hf-task-dplus
 DOO2_TASK_LC=0      # hf-task-lc
 DOO2_TASK_XIC=0     # hf-task-xic
