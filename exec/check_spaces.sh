210d620651416b7ae9b56ea804f5ce68e9c88fd6
Vít Kučera <vit.kucera@cern.ch>
2021-05-19 03:09:01 +0200

    Add detection of trailing whitespaces in the CI tests

--- exec/check_spaces.sh
+++ exec/check_spaces.sh
@@ -2,18 +2,26 @@
 
 # Find tabs in text files
 
-status=0
+status_tab=0
+status_trail=0
 # loop over files
 while IFS= read -r f; do
   # ignore binary files
   file -bi "$f" | grep -q "charset=binary" && continue
-  # find tabs in file
   echo "Scanning file: $f"
+  # find tabs in file
   if grep -q -P "\t" "$f"; then
-    status=1
+    status_tab=1
     echo "Found some tabs in $f:" > /dev/stderr
     # print out where the tabs are
     grep -P -n -C 1 "\t" "$f" > /dev/stderr
   fi
+  # find trailing whitespaces in file
+  if grep -q " $" "$f"; then
+    status_trail=2
+    echo "Found some trailing whitespaces in $f:" > /dev/stderr
+    # print out where the tabs are
+    grep -n -C 1 " $" "$f" > /dev/stderr
+  fi
 done < <(find . -type f -not -path "./.git/*")
-exit $status
+exit $((status_tab + status_trail))
