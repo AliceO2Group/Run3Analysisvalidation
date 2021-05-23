e264d7671a8db7dcd4af61a5e95264eea5f60028
Vít Kučera <vit.kucera@cern.ch>
2021-05-24 01:16:45 +0200

    Improve reports of space checker CI and script (#226)
    * CI: Improve space checker report
    
    * Improve space checking script report.

--- exec/check_spaces.sh
+++ exec/check_spaces.sh
@@ -2,6 +2,13 @@
 
 # Find tabs in text files
 
+# This directory
+DIR_THIS="$(dirname "$(realpath "$0")")"
+
+# Load utilities.
+# shellcheck disable=SC1091 # Ignore not following.
+source "$DIR_THIS/utilities.sh" || { echo "Error: Failed to load utilities."; exit 1; }
+
 status_tab=0
 status_trail=0
 # loop over files
@@ -12,16 +19,23 @@ while IFS= read -r f; do
   # find tabs in file
   if grep -q -P "\t" "$f"; then
     status_tab=1
-    echo "Found some tabs in $f:" > /dev/stderr
+    MsgErr "Found some tabs in $f:" > /dev/stderr
     # print out where the tabs are
-    grep -P -n -C 1 "\t" "$f" > /dev/stderr
+    #grep -P -n "\t" "$f" > /dev/stderr
+    awk 'i=index($0, "\t") {printf "%i (col. %i): %s\n", NR, i, $0}' "$f" > /dev/stderr
   fi
   # find trailing whitespaces in file
   if grep -q " $" "$f"; then
     status_trail=2
-    echo "Found some trailing whitespaces in $f:" > /dev/stderr
+    MsgErr "Found some trailing whitespaces in $f:" > /dev/stderr
     # print out where the tabs are
-    grep -n -C 1 " $" "$f" > /dev/stderr
+    grep -n " $" "$f" > /dev/stderr
   fi
 done < <(find . -type f -not -path "./.git/*")
-exit $((status_tab + status_trail))
+status=$((status_tab + status_trail))
+if [ "$status" -ne 0 ]; then
+  MsgWarn "Command tips:
+  - Replace each tab with two spaces: sed -i 's/\\\t/  /g' <files>
+  - Remove trailing whitespaces: sed -i 's/[[:space:]]*$//' <files>"
+fi
+exit $status
