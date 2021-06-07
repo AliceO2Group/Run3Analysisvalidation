6677c8ebdff3fe083933083851c53ec206c3c235
Gian Michele Innocenti <ginnocen@alicecerno2.cern.ch>
2021-06-07 11:38:27 +0200

    update input files

--- codeHF/config_input.sh
+++ codeHF/config_input.sh
@@ -11,10 +11,10 @@ NFILESMAX=1             # Maximum number of processed input files. (Set to -0 to
 # Number of input files per job (Automatic optimisation on if < 1.)
 NFILESPERJOB_CONVERT=0  # Conversion
 NFILESPERJOB_ALI=0      # AliPhysics
-NFILESPERJOB_O2=1       # O2
+NFILESPERJOB_O2=2       # O2
 
 # Maximum number of simultaneously running O2 jobs
-NJOBSPARALLEL_O2=$(python3 -c "print(min(10, round($(nproc) / 2)))")
+NJOBSPARALLEL_O2=$(python3 -c "print(min(20, round($(nproc) / 2)))")
 
 JSONRUN3="dpl-config_run3.json"  # Run 3 tasks parameters
 # Run 5 tasks parameters for open HF study
@@ -53,7 +53,7 @@ case $INPUT_CASE in
     ISMC=1;;
   7)
     INPUT_LABEL="Run 5, p-p MC 14 TeV Inel, Scenario 3, HF analysis"
-    INPUT_DIR="/data/Run5/MC/pp_14TeV/Inel_v1"
+    INPUT_DIR="/data/Run5/MC/Inel_S3_Werner_120k_20210422"
     INPUT_FILES="AODRun5.*.root"
     JSON="$JSONRUN5_HF"
     ISINPUTO2=1
@@ -90,6 +90,14 @@ case $INPUT_CASE in
     INPUT_LABEL="Run 5, p-p MC 14 TeV Multicharm enriched, Scenario 3, HF analysis setup using dpl-config_run5_hf.json"
     #INPUT_DIR="/home/ginnocen/alice/DelphesO2/examples/scripts"
     INPUT_DIR="/home/mmazzill/pp14TeV_XiccGun_ccbarv1_20M__01062021"
+    INPUT_FILES="AODRun5.*.root"
+    JSON="$JSONRUN5_HF"
+    ISINPUTO2=1
+    ISMC=1;;
+  13)
+    INPUT_LABEL="Run 5, p-p MC 14 TeV Multicharm GunBox, Scenario 3, HF analysis setup using dpl-config_run5_hf.json"
+    #INPUT_DIR="/home/ginnocen/alice/DelphesO2/examples/scripts"
+    INPUT_DIR="/data/Run5/MC/pp_14TeV/MultiCharm_S3_20210607_GunBox_0p5T"
     INPUT_FILES="AODRun5.*.root"
     JSON="$JSONRUN5_HF"
     ISINPUTO2=1
