2ac991a3938d30d735035b80382c6c0753cbdab8
Vít Kučera <vit.kucera@cern.ch>
2021-06-12 01:22:59 +0200

    Simplify batch scripts

--- exec/batch_o2.sh
+++ exec/batch_o2.sh
@@ -29,13 +29,14 @@ LogFile="log_o2.log"
 ListIn="list_o2.txt"
 FilesToMerge="ListOutToMergeO2.txt"
 FilesToMergeTree="ListOutToMergeO2Tree.txt"
-DirBase="$PWD"
 IndexFile=0
-ListRunScripts="$DirBase/ListRunScriptsO2.txt"
+IndexJob=0
 DirOutMain="output_o2"
 
+CMDPARALLEL="cd \"$DirOutMain/{}\" && bash \"$DIR_THIS/run_o2.sh\" \"$SCRIPT\" \"$ListIn\" \"$JSON\" \"$LogFile\""
+
 # Clean before running.
-rm -rf "$ListRunScripts" "$FilesToMerge" "$FilesToMergeTree" "$FILEOUT" "$FILEOUT_TREE" "$DirOutMain" || ErrExit "Failed to delete output files."
+rm -rf "$FilesToMerge" "$FilesToMergeTree" "$FILEOUT" "$FILEOUT_TREE" "$DirOutMain" || ErrExit "Failed to delete output files."
 
 CheckFile "$LISTINPUT"
 echo "Output directory: $DirOutMain (logfiles: $LogFile)"
@@ -48,33 +49,26 @@ while read -r FileIn; do
   # New job
   if [ $((IndexFile % NFILESPERJOB)) -eq 0 ]; then
     mkdir -p $DirOut || ErrExit "Failed to mkdir $DirOut."
-    FileOut="$DirOut/$FILEOUT"
-    echo "$FileOut" >> "$DirBase/$FilesToMerge" || ErrExit "Failed to echo to $DirBase/$FilesToMerge."
-    [ "$FILEOUT_TREE" ] && {
-      FileOutTree="$DirOut/$FILEOUT_TREE"
-      echo "$FileOutTree" >> "$DirBase/$FilesToMergeTree" || ErrExit "Failed to echo to $DirBase/$FilesToMergeTree."
-    }
-    # Add this job in the list of commands.
-    echo "cd \"$DirOut\" && bash \"$DIR_THIS/run_o2.sh\" \"$SCRIPT\" \"$ListIn\" \"$JSON\" \"$LogFile\"" >> "$ListRunScripts" || ErrExit "Failed to echo to $ListRunScripts."
+    echo "$DirOut/$FILEOUT" >> "$FilesToMerge" || ErrExit "Failed to echo to $FilesToMerge."
+    [ "$FILEOUT_TREE" ] && \
+    { echo "$DirOut/$FILEOUT_TREE" >> "$FilesToMergeTree" || ErrExit "Failed to echo to $FilesToMergeTree."; }
   fi
   echo "$FileIn" >> "$DirOut/$ListIn" || ErrExit "Failed to echo to $DirOut/$ListIn."
   [ "$DEBUG" -eq 1 ] && echo "Input file ($IndexFile, job $IndexJob): $FileIn"
-  ((IndexFile+=1))
+  ((IndexFile++))
 done < "$LISTINPUT"
 
-CheckFile "$ListRunScripts"
-echo "Running O2 jobs... ($(wc -l < "$ListRunScripts") jobs, $NJOBSPARALLEL parallel, $NFILESPERJOB files/job)"
+echo "Running O2 jobs... ($((IndexJob+1)) jobs, $NJOBSPARALLEL parallel, $NFILESPERJOB files/job)"
 OPT_PARALLEL="--halt soon,fail=100% --jobs $NJOBSPARALLEL"
 if [ "$DEBUG" -eq 0 ]; then
   # shellcheck disable=SC2086 # Ignore unquoted options.
-  parallel $OPT_PARALLEL < "$ListRunScripts" > $LogFile 2>&1
+  parallel $OPT_PARALLEL "$CMDPARALLEL" ::: $(seq 0 $IndexJob) > $LogFile 2>&1
 else
   # shellcheck disable=SC2086 # Ignore unquoted options.
-  parallel $OPT_PARALLEL --will-cite --progress < "$ListRunScripts" > $LogFile
+  parallel $OPT_PARALLEL --will-cite --progress "$CMDPARALLEL" ::: $(seq 0 $IndexJob) > $LogFile
 fi || ErrExit "\nCheck $(realpath $LogFile)"
 grep -q "\\[WARN\\]" "$LogFile" && MsgWarn "There were warnings!\nCheck $(realpath $LogFile)"
 grep -q -e "\\[ERROR\\]" -e "segmentation" "$LogFile" && MsgErr "There were errors!\nCheck $(realpath $LogFile)"
-rm -f "$ListRunScripts" || ErrExit "Failed to rm $ListRunScripts."
 
 echo "Merging output files... (output file: $FILEOUT, logfile: $LogFile)"
 hadd $FILEOUT @"$FilesToMerge" >> $LogFile 2>&1 || \
