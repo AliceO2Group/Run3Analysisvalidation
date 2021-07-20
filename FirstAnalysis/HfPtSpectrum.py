7c398f6be0c00d9c28b36a07be60b5c2b89dbe98
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2021-07-20 17:48:42 +0200

    Add plot

--- FirstAnalysis/HfPtSpectrum.py
+++ FirstAnalysis/HfPtSpectrum.py
@@ -19,10 +19,14 @@ author: Fabrizio Grosa <fabrizio.grosa@cern.ch>, CERN
 
 
 import sys
+import os
 import argparse
 import numpy as np
 import yaml
+from hfplot.plot_spec_root import ROOTFigure
+from hfplot.style import StyleObject1D
 from ROOT import TFile, TH1F, TH1, TGraphAsymmErrors # pylint: disable=import-error,no-name-in-module
+from ROOT import gROOT, kAzure, kFullCircle # pylint: disable=import-error,no-name-in-module
 from HfAnalysisUtils import ComputeCrossSection, ComputeFractionFc, ComputeFractionNb, GetHistoBinLimits
 
 
@@ -32,11 +36,11 @@ def LoadInputs(inputCfg):
 
   Parameters
   ----------
-  - inputCfg: dictionary from yaml config file 
+  - inputCfg: dictionary from yaml config file (see example https://github.com/AliceO2Group/Run3Analysisvalidation/blob/master/FirstAnalysis/config/config_ptspectrum_Dplus_pp5TeV.yml)
 
   Returns
   ----------
-  - histos: dictionary with input histos, keys (rawyidls, acceffp, acceffnp)
+  - histos: dictionary with input histos, keys (rawyields, acceffp, acceffnp)
   - norm: dictionary with normalisations, keys (BR, events, sigmaMB)
   '''
 
@@ -116,7 +120,7 @@ def LoadInputs(inputCfg):
       histos['FONLL']['prompt'][pred] = inFileFONLL.Get(f'{FONLLhistoName[channel]}pred_{pred}')
       histos['FONLL']['prompt'][pred].SetDirectory(0)
   inFileFONLL.Close()
-  
+
   # load normalisation info from common database
   norm = {}
   with open('config/norm_database.yml', 'r') as ymlNormDB:
@@ -137,7 +141,13 @@ def Main():
   parser.add_argument('configFileName', metavar='text',
                       default='config_Dplus_pp5TeV.yml',
                       help='input yaml config file')
+  parser.add_argument('--batch', action='store_true',
+                      default=False,
+                      help='suppress video output')
   args = parser.parse_args()
+
+  if args.batch:
+    gROOT.SetBatch(True)
 
   # load info from config file
   with open(args.configFileName, 'r') as ymlConfigFile:
@@ -155,11 +165,15 @@ def Main():
       sys.exit()
 
   # compute cross section
+  axisTitCross = 'd#sigma/d#it{p}_{T} (pb GeV^{-1} #it{c})'
+  axisTitCrossTimesBR = 'd#sigma/d#it{p}_{T} #times BR (pb GeV^{-1} #it{c})'
+  axisTitPt = '#it{p}_{T} (GeV/#it{c})'
+  axisTitFprompt = '#if{f}_{prompt}'
   gFraction = TGraphAsymmErrors(0)
-  gFraction.SetNameTitle('gFraction', ';#it{p}_{T} (GeV/#it{c});#if{f}_{prompt}')
-  hPtSpectrum = TH1F('hPtSpectrum', ';#it{p}_{T} (GeV/#it{c});d#sigma/d#it{p}_{T} (pb GeV^{-1} #it{c})',
+  gFraction.SetNameTitle('gFraction', f';{axisTitPt};{axisTitFprompt}')
+  hPtSpectrum = TH1F('hPtSpectrum', f';{axisTitPt};{axisTitCross}',
                      len(ptLims['rawyields'])-1, ptLims['rawyields'])
-  hPtSpectrumWoBR = TH1F('hPtSpectrumWoBR', ';#it{p}_{T} (GeV/#it{c});d#sigma/d#it{p}_{T} #times BR (pb GeV^{-1} #it{c})',
+  hPtSpectrumWoBR = TH1F('hPtSpectrumWoBR', f';{axisTitPt};{axisTitCrossTimesBR}',
                          len(ptLims['rawyields'])-1, ptLims['rawyields'])
 
   for iPt, (ptMin, ptMax) in enumerate(zip(ptLims['rawyields'][:-1], ptLims['rawyields'][1:])):
@@ -173,6 +187,8 @@ def Main():
     ptMaxFONLL = histos['FONLL']['nonprompt']['central'].GetXaxis().FindBin(ptMax*0.9999)
     crossSecNonPromptFONLL = [histos['FONLL']['nonprompt'][pred].Integral(ptMinFONLL, ptMaxFONLL, 'width') /
                               (ptMax - ptMin) for pred in histos['FONLL']['nonprompt']]
+
+    # compute prompt fraction
     if fracMethod == 'Nb':
       frac = ComputeFractionNb(rawY, effTimesAccPrompt, effTimesAccNonPrompt, crossSecNonPromptFONLL, ptDelta, 1.,
                                1., norm['events'], norm['sigmaMB']) # BR already included in FONLL prediction
@@ -181,6 +197,7 @@ def Main():
                              (ptMax - ptMin) for pred in histos['FONLL']['prompt']]
       frac = ComputeFractionFc(effTimesAccPrompt, effTimesAccNonPrompt, crossSecPromptFONLL, crossSecNonPromptFONLL)
 
+    # compute cross section times BR
     crossSec, crossSecUnc = ComputeCrossSection(rawY, uncRawY, frac[0], effTimesAccPrompt, ptMax-ptMin, 1.,
                                                 norm['sigmaMB'], norm['events'], 1., fracMethod)
 
@@ -191,7 +208,30 @@ def Main():
     gFraction.SetPoint(iPt, ptCent, frac[0])
     gFraction.SetPointError(iPt, ptDelta/2, ptDelta/2, frac[0]-frac[1], frac[2]-frac[0])
 
-  outFile = TFile(cfg['output']['filename'], 'recreate')
+  # create plots
+  styleHist = StyleObject1D()
+  styleHist.markercolor = kAzure+4
+  styleHist.markerstyle = kFullCircle
+  styleHist.markersize = 1
+  styleHist.linecolor = kAzure+4
+  styleHist.linewidth = 2
+  styleHist.draw_options = 'P'
+
+  figCrossSec = ROOTFigure(1, 1, column_margin=(0.14, 0.035), row_margin=(0.1, 0.035), size=(600, 800))
+  figCrossSec.axes(label_size=0.025, title_size=0.030)
+  figCrossSec.axes('x', title=axisTitPt, title_offset=1.5)
+  figCrossSec.axes('y', title=axisTitCrossTimesBR, title_offset=1.8)
+  figCrossSec.define_plot(0, 0, y_log=True)
+  figCrossSec.add_object(hPtSpectrumWoBR, style=styleHist)
+  figCrossSec.create()
+
+  outputDir = cfg['output']['directory']
+  if not os.path.exists(outputDir):
+      os.makedirs(outputDir)
+  figCrossSec.save(os.path.join(outputDir, f'{cfg["output"]["filename"]}.pdf'))
+
+  # save output file
+  outFile = TFile(os.path.join(outputDir, f'{cfg["output"]["filename"]}.root'), 'recreate')
   hPtSpectrum.Write()
   hPtSpectrumWoBR.Write()
   gFraction.Write()
