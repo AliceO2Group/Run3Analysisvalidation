bf171ec63afe16bb3fb2f55c1b49ced31488cc29
Jseo <47848181+JinjooSeo@users.noreply.github.com>
2021-09-09 00:44:24 +0900

    Add Ξcc tree creator workflow (#288)
    * Add Ξcc tree workflow
    
    * Add Ξcc tree workflow

--- codeHF/config_tasks.sh
+++ codeHF/config_tasks.sh
@@ -69,6 +69,7 @@ DOO2_TASK_XICC=0    # hf-task-xicc
 DOO2_TREE_D0=0      # hf-tree-creator-d0-tokpi
 DOO2_TREE_LC=0      # hf-tree-creator-lc-topkpi
 DOO2_TREE_X=0       # hf-tree-creator-x-tojpsipipi
+DOO2_TREE_XICC=0    # hf-tree-creator-xicc-topkpipi
 # Correlations
 DOO2_D0D0BAR_DATA=0      # hf-correlator-d0d0bar
 DOO2_D0D0BAR_MCREC=0     # hf-correlator-d0d0bar-mc-rec
@@ -229,6 +230,7 @@ function MakeScriptO2 {
   [ $DOO2_TREE_D0 -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-tree-creator-d0-tokpi"
   [ $DOO2_TREE_LC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-tree-creator-lc-topkpi"
   [ $DOO2_TREE_X -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-tree-creator-x-tojpsipipi"
+  [ $DOO2_TREE_XICC -eq 1 ] && WORKFLOWS+=" o2-analysis-hf-tree-creator-xicc-topkpipi"
 
   # Translate options into arguments of the generating script.
   OPT_MAKECMD=""
