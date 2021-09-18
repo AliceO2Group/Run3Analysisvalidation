75d01babfc3c3591b70f95fa461c4eca4572b44f
vkucera <vkucera@users.noreply.github.com>
2021-09-18 15:35:28 +0000

    [Mega-Linter] Apply linters fixes

--- FirstAnalysis/sig_vs_bkg.py
+++ FirstAnalysis/sig_vs_bkg.py
@@ -275,7 +275,7 @@ rebin = 1
 path_file_sig = "../codeHF/AnalysisResults_O2.root"
 path_file_bkg = "../codeHF/AnalysisResults_O2.root"
 
-#variables = ["d0Prong0", "d0Prong1", "d0Prong2", "PtProng0", "PtProng1", "PtProng2", "CPA", "Eta", "Declength"]
+# variables = ["d0Prong0", "d0Prong1", "d0Prong2", "PtProng0", "PtProng1", "PtProng2", "CPA", "Eta", "Declength"]
 variables = ["CPA", "Pt", "Eta"]
 
 decays = ["d0"]
