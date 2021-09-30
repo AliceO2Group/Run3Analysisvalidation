33f2e31c74207ded011b72ca75401dbcd82d6517
Vít Kučera <vit.kucera@cern.ch>
2021-09-30 18:20:54 +0200

    Fix physics selection in RunHFTaskLocal.C

--- codeHF/RunHFTaskLocal.C
+++ codeHF/RunHFTaskLocal.C
@@ -35,9 +35,7 @@ Long64_t RunHFTaskLocal(TString txtfile = "./list_ali.txt",
   handler->SetReadTR(kFALSE);
   mgr->SetMCtruthEventHandler(handler);
 
-  //Apply the event selection
-  gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
-
+  // Apply the event selection
   AliPhysicsSelectionTask* physSelTask = reinterpret_cast<AliPhysicsSelectionTask*>(gInterpreter->ProcessLine(Form(".x %s(%d)", gSystem->ExpandPathName("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C"), isMC)));
 
   AliAnalysisTaskHFSimpleVertices* tasktr3 = reinterpret_cast<AliAnalysisTaskHFSimpleVertices*>(gInterpreter->ProcessLine(Form(".x %s(\"\",\"%s\",%d)", gSystem->ExpandPathName("$ALICE_PHYSICS/PWGHF/vertexingHF/macros/AddTaskHFSimpleVertices.C"), jsonfilename.Data(), isMC)));
