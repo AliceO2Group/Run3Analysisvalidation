994cf3a98159a8d59a05a76055d6fe62c36128e1
Vít Kučera <vit.kucera@cern.ch>
2022-10-09 19:29:58 +0200

    Clean files with any extension

--- codeHF/clean.sh
+++ codeHF/clean.sh
@@ -4,23 +4,23 @@
 
 rm -rf \
 AnalysisResults_ALI.root AnalysisResults_O2.root AnalysisResults_trees_O2.root \
-comparison_histos_tracks.pdf comparison_ratios_tracks.pdf \
-comparison_histos_skim.pdf comparison_ratios_skim.pdf \
-comparison_histos_cand2.pdf comparison_ratios_cand2.pdf \
-comparison_histos_cand3.pdf comparison_ratios_cand3.pdf \
-comparison_histos_d0.pdf comparison_ratios_d0.pdf \
-comparison_histos_d0-mc.pdf comparison_ratios_d0-mc.pdf \
-comparison_histos_dplus.pdf comparison_ratios_dplus.pdf \
-comparison_histos_lc.pdf comparison_ratios_lc.pdf \
-comparison_histos_lc-mc.pdf comparison_ratios_lc-mc.pdf \
-comparison_histos_jpsi.pdf comparison_ratios_jpsi.pdf \
-MC_d0_eff.pdf MC_d0_pT.pdf \
-MC_dplus_eff.pdf MC_dplus_pT.pdf \
-MC_lc_eff.pdf MC_lc_pT.pdf \
-MC_xic_eff.pdf MC_xic_pT.pdf \
-MC_xicc_eff.pdf MC_xicc_pT.pdf \
-MC_jpsi_eff.pdf MC_jpsi_pT.pdf \
-MC_lc-tok0sP_eff.pdf MC_lc-tok0sP_pT.pdf \
+comparison_histos_tracks.* comparison_ratios_tracks.* \
+comparison_histos_skim.* comparison_ratios_skim.* \
+comparison_histos_cand2.* comparison_ratios_cand2.* \
+comparison_histos_cand3.* comparison_ratios_cand3.* \
+comparison_histos_d0.* comparison_ratios_d0.* \
+comparison_histos_d0-mc.* comparison_ratios_d0-mc.* \
+comparison_histos_dplus.* comparison_ratios_dplus.* \
+comparison_histos_lc.* comparison_ratios_lc.* \
+comparison_histos_lc-mc.* comparison_ratios_lc-mc.* \
+comparison_histos_jpsi.* comparison_ratios_jpsi.* \
+MC_d0_eff.* MC_d0_pT.* \
+MC_dplus_eff.* MC_dplus_pT.* \
+MC_lc_eff.* MC_lc_pT.* \
+MC_xic_eff.* MC_xic_pT.* \
+MC_xicc_eff.* MC_xicc_pT.* \
+MC_jpsi_eff.* MC_jpsi_pT.* \
+MC_lc-tok0sP_eff.* MC_lc-tok0sP_pT.* \
 ./*.log \
 output_* \
 || { echo "Error: Failed to delete files."; exit 1; }
