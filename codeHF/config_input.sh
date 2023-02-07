ccb012cb6f57d4eca4d89604a9095a6a6ef4f3fb
Vít Kučera <vit.kucera@cern.ch>
2023-02-07 20:00:02 +0100

    Format bash cases

--- codeHF/config_input.sh
+++ codeHF/config_input.sh
@@ -38,48 +38,58 @@ INPUT_BASE="/data2/data" # alicecerno2
 case $INPUT_CASE in
   1)
     INPUT_LABEL="Run 2, p-p 5.02 TeV LHC17p, real"
-    INPUT_DIR="$INPUT_BASE/Run2/pp_5.02TeV/real/LHC17p_pass1_CENT_woSDD";;
+    INPUT_DIR="$INPUT_BASE/Run2/pp_5.02TeV/real/LHC17p_pass1_CENT_woSDD"
+    ;;
   2) # reference
     INPUT_LABEL="Run 2, p-p 5.02 TeV LHC17p, MC LHC18a4a2_cent"
     INPUT_DIR="$INPUT_BASE/Run2/pp_5.02TeV/sim/LHC18a4a2_cent/282099"
-    ISMC=1;;
+    ISMC=1
+    ;;
   3)
     INPUT_LABEL="Run 2, p-p 5.02 TeV LHC17p, MC LHC18a4a2_cent"
     INPUT_DIR="$INPUT_BASE/Run2/pp_5.02TeV/sim/LHC18a4a2_cent/282341"
-    ISMC=1;;
+    ISMC=1
+    ;;
   4)
     INPUT_LABEL="Run 2, Pb-Pb 5.02 TeV LHC15o, real"
     INPUT_DIR="$INPUT_BASE/Run2/PbPb_5.02TeV/real/LHC15o"
-    INPUT_SYS="PbPb";;
+    INPUT_SYS="PbPb"
+    ;;
   5)
     INPUT_LABEL="Run 2, Pb-Pb 5.02 TeV LHC15o, MC LHC15k1a3"
     INPUT_DIR="$INPUT_BASE/Run2/PbPb_5.02TeV/sim/LHC15k1a3"
     INPUT_SYS="PbPb"
-    ISMC=1;;
+    ISMC=1
+    ;;
   6)
     INPUT_LABEL="Run 2, p-p 13 TeV LHC16p, MC LHC19g6f3, dedicated Ξc"
     INPUT_DIR="$INPUT_BASE/Run2/pp_13TeV/sim/LHC19g6f3"
-    ISMC=1;;
+    ISMC=1
+    ;;
   7)
     INPUT_LABEL="Run 3, p-p 13.6 TeV, MC LHC21k6, general purpose"
     INPUT_DIR="$INPUT_BASE/Run3/pp_13.6TeV/sim/LHC21k6/302028/AOD"
     INPUT_FILES="AO2D.root"
     ISINPUTO2=1
     INPUT_RUN=3
-    ISMC=1;;
+    ISMC=1
+    ;;
   8)
     INPUT_LABEL="Run 2, p-p 13 TeV LHC18f, MC LHC20f4a (ESD)"
     INPUT_DIR="$INPUT_BASE/Run2/pp_13TeV/sim/LHC20f4a"
-    ISMC=1;;
+    ISMC=1
+    ;;
   9)
     INPUT_LABEL="Run 2, p-p 13 TeV LHC18f, MC LHC20f4a (AO2D)"
     INPUT_DIR="$INPUT_BASE/Run2/pp_13TeV/sim_converted/LHC20f4a"
     INPUT_FILES="AO2D.root"
     ISINPUTO2=1
-    ISMC=1;;
+    ISMC=1
+    ;;
   10)
     INPUT_LABEL="Run 2, p-p 13 TeV, LHC17j (AO2D)"
     INPUT_DIR="$INPUT_BASE/Run2/pp_13TeV/real_converted/LHC17j_20220601" # converted good AO2Ds
     INPUT_FILES="AO2D.root"
-    ISINPUTO2=1;;
+    ISINPUTO2=1
+    ;;
   esac
