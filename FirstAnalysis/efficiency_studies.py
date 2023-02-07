06a1362fe2fda2b7f5694730ce3cf9e7fbe65f6f
Maja Kabus <mkabus@cern.ch>
2023-02-07 15:58:56 +0100

    Efficiency studies: Simplified plots, adjusted bins and style to compare with AliPhysics (#388)
    * Simplified plots, adjusted bins and style to compare with AliPhysics
    
    * Drop All which wont work with O2Physics master
    
    * Minor improvements to plotting efficiency
    
    * Get rid of one more pylint error
    
    * Inverse comparison with err_y
    
    * Add option to draw efficiency histograms for pos+neg together
    
    * Primaries for all variables
    
    * [MegaLinter] Apply linters fixes
    
    ---------
    
    Co-authored-by: saganatt <saganatt@users.noreply.github.com>

--- FirstAnalysis/efficiency_studies.py
+++ FirstAnalysis/efficiency_studies.py
@@ -2,14 +2,26 @@
 """
 file: efficiency_studies.py
 brief: Plotting macro for the qa-efficiency task.
-usage: ./efficiency_studies.py AnalysisResults_O2.root
+usage: ./efficiency_studies.py AnalysisResults_O2.root [--plot_sel | --dump_eff]
 author: Maja Kabus <mkabus@cern.ch>, CERN / Warsaw University of Technology
 """
 
 import argparse
 
-from ROOT import TH1F, TCanvas, TEfficiency, TFile, TLegend, TLatex # pylint: disable=import-error,no-name-in-module
-from ROOT import gPad, gStyle, gROOT # pylint: disable=import-error,no-name-in-module
+from ROOT import (  # pylint: disable=import-error,no-name-in-module
+    TH1F,
+    TCanvas,
+    TEfficiency,
+    TFile,
+    TLatex,
+    TLegend,
+    gPad,
+    gROOT,
+    gStyle,
+    kGreen,
+    kOrange,
+)
+
 
 def save_canvas(canvas, title):
     """
@@ -20,25 +32,41 @@ def save_canvas(canvas, title):
         canvas.SaveAs(title + file_format)
 
 
-def prepare_canvas(var, sign, had, det): # pylint: disable=too-many-locals
+def get_titles(var, sign, had, det):
+    """
+    Compose titles and names for histograms and canvas.
+    """
+    hname = f"hempty_{sign}_{had}_{det}_{var}"
+    cname = f"c_{sign}_{had}_{det}_{var}"
+    ctitle = f"Efficiency for {sign} {had}, {det} primaries"
+    return hname, cname, ctitle
+
+
+def prepare_canvas(var, titles):
     """
     Initialize canvas, axes, legend.
     `hempty` must be captured at return, otherwise ROOT crashes.
     """
-    cname = f"c_{had}_{det}_{sign}_{var}"
-    hname = f"hempty_{had}_{det}_{sign}_{var}"
-    ctitle = f"Efficiency for {sign} {had}, {det}"
-    if var == "Pt":
-        ctitle = f"{ctitle} primaries"
+    hname, cname, ctitle = titles
 
     def get_pt_hist():
-        hempty = TH1F(hname, f"{ctitle};Transverse Momentum (GeV/c);Efficiency", 100, 0.05, 10)
-        gPad.SetLogx()
+        hempty = TH1F(
+            hname, f"{ctitle};Transverse Momentum (GeV/c);Efficiency", 16, 0.00, 16
+        )
+        # gPad.SetLogx()
         return hempty
+
     def get_eta_hist():
-        return TH1F(hname, f"{ctitle};Pseudorapidity;Efficiency", 100, -1.5, 1.5)
+        return TH1F(hname, f"{ctitle};Pseudorapidity;Efficiency", 16, -1.5, 1.5)
+
     def get_phi_hist():
-        return TH1F(hname, f"{ctitle};Azimuthal angle (rad);Efficiency", 100, 0.0, 6.0)
+        return TH1F(
+            hname,
+            f"{ctitle};Azimuthal angle (rad);Efficiency",
+            16,
+            -2 * 3.1416 - 0.5,
+            2 * 3.1416 + 0.5,
+        )
 
     hists = {"Pt": get_pt_hist, "Eta": get_eta_hist, "Phi": get_phi_hist}
 
@@ -53,20 +81,15 @@ def prepare_canvas(var, sign, had, det): # pylint: disable=too-many-locals
     hempty.GetXaxis().CenterTitle()
     hempty.GetXaxis().SetNoExponent()
     hempty.GetXaxis().SetMoreLogLabels(1)
+    hempty.GetYaxis().SetNdivisions(11)
     hempty.Draw()
 
-    xmin = hempty.GetXaxis().GetXmin()
-    xmax = hempty.GetXaxis().GetXmax()
-    ymax = hempty.GetYaxis().GetXmax()
     latexa = TLatex()
     latexa.SetTextSize(0.04)
     latexa.SetTextFont(42)
     latexa.SetTextAlign(3)
-    xave = xmin + (xmax - xmin) / 4.0
-    latexa.DrawLatex(
-        xave, ymax * 0.2, "-0.8 #geq #eta #geq 0.8"
-    )
-    latexa.DrawLatex(xave, ymax * 0.15, "0.00 #geq #varphi #geq 2#pi")
+    latexa.DrawLatexNDC(0.15, 0.3, "-0.9 #geq #eta #geq 0.9")
+    latexa.DrawLatexNDC(0.15, 0.25, "-2#pi #geq #varphi #geq 2#pi")
 
     leg = TLegend(0.55, 0.15, 0.89, 0.35, "P")
     leg.SetNColumns(2)
@@ -75,60 +98,102 @@ def prepare_canvas(var, sign, had, det): # pylint: disable=too-many-locals
 
     return canv, leg, hempty
 
-def efficiency_tracking(fileo2, det, sign, var): # pylint: disable=too-many-locals
+
+def get_efficiency_all_charges(infile, det, var, had):
+    """
+    Calculate efficiency for positive and negative particles together.
+    """
+    det_str = det.lower().replace("-", "_")
+    prm_str = "" if var == "Phi" else "prm/"
+    num_pos = infile.Get(f"qa-efficiency/MC/{had}/pos/{var.lower()}/{prm_str}{det_str}")
+    num_neg = infile.Get(f"qa-efficiency/MC/{had}/neg/{var.lower()}/{prm_str}{det_str}")
+    den_pos = infile.Get(f"qa-efficiency/MC/{had}/pos/{var.lower()}/{prm_str}generated")
+    den_neg = infile.Get(f"qa-efficiency/MC/{had}/neg/{var.lower()}/{prm_str}generated")
+    num = num_pos.Clone()
+    num.Add(num_neg)
+    den = den_pos.Clone()
+    den.Add(den_neg)
+
+    axis = num.GetXaxis()
+    efftitle = f"Efficiency for {had}, {det} primaries;{axis.GetTitle()};Efficiency"
+    if axis.IsVariableBinSize():
+        eff = TEfficiency(
+            f"{det}_vs{var}_Prm", efftitle, axis.GetNbins(), axis.GetXbins().GetArray()
+        )
+    else:
+        eff = TEfficiency(
+            f"{det}_vs{var}_Prm",
+            efftitle,
+            axis.GetNbins(),
+            axis.GetXmin(),
+            axis.GetXmax(),
+        )
+
+    eff.SetTotalHistogram(den, "f")
+    eff.SetPassedHistogram(num, "f")
+    return eff
+
+
+def efficiency_tracking(
+    infile, heff, det, sign, var, err_y
+):  # pylint: disable=too-many-locals, too-many-arguments
     """
     Plot efficiency vs pT, eta and phi for all hadron species.
+    Pt plots are drawn for primaries.
+    If sign = "All", efficiency is plot for pos+neg together.
     """
-    hadron_list = ["Pion", "Kaon", "Proton", "All"]
+    hadron_list = ["All", "Pion", "Kaon", "Proton", "Electron"]
+    hist_hadron_list = ["all", "pi", "ka", "pr", "el"]
     # Other hadrons: "Deuteron", "Triton", "He3", "Alpha"
-    color_list = [1, 2, 4, 6, 8, 28]
-    marker_list = [20, 21, 22, 34, 45, 47]
-    eff_objs = {"Pt": f"{det}_vsPt_Prm", "Eta": f"{det}_vsEta", "Phi": f"{det}_vsPhi"}
+    color_list = [1, 2, 4, kGreen + 2, kOrange - 3]
+    marker_list = [20, 21, 22, 21, 22]
+    eff_objs = {
+        "Pt": f"{det}_vsPt_Prm",
+        "Eta": f"{det}_vsEta_Prm",
+        "Phi": f"{det}_vsPhi_Prm",
+    }
 
     gStyle.SetOptStat(0)
-    gStyle.SetErrorX(0)
     gStyle.SetFrameLineWidth(2)
     gStyle.SetTitleSize(0.045, "x")
     gStyle.SetTitleSize(0.045, "y")
     gStyle.SetMarkerSize(1)
     gStyle.SetLabelOffset(0.015, "x")
     gStyle.SetLabelOffset(0.02, "y")
-    gStyle.SetTickLength(-0.02, "x")
-    gStyle.SetTickLength(-0.02, "y")
     gStyle.SetTitleOffset(1.1, "x")
     gStyle.SetTitleOffset(1.0, "y")
 
-    results = prepare_canvas(var, sign, "all", det)
+    results = prepare_canvas(var, get_titles(var, sign, "all", det))
     c_all = results[0]
     leg_all = results[1]
     eff_list = []
 
-    heff = fileo2.Get("qa-efficiency/EfficiencyMC")
-    for i, had in enumerate(hadron_list):
-        leff = heff.FindObject(f"{sign} {had}")
-        eff = leff.FindObject(eff_objs[var])
-        results_single = prepare_canvas(var, sign, had, det)
-        c_single = results_single[0]
-        c_single.cd()
+    for i, (had, hhad) in enumerate(zip(hadron_list, hist_hadron_list)):
+        if sign == "All":
+            eff = get_efficiency_all_charges(infile, det, var, hhad)
+        else:
+            eff = heff.FindObject(f"{sign} {had}").FindObject(eff_objs[var])
+        c_all.cd()
         eff.Paint("p")
         graph = eff.GetPaintedGraph().Clone()
-        c_single.GetListOfPrimitives().Remove(eff)
-        c_single.Modified()
+        c_all.GetListOfPrimitives().Remove(eff)
+        c_all.Modified()
         for j in range(0, graph.GetN()):
             graph.GetEXlow()[j] = 0
             graph.GetEXhigh()[j] = 0
+            if not err_y:
+                graph.GetEYlow()[j] = 0
+                graph.GetEYhigh()[j] = 0
 
         graph.SetLineColor(color_list[i])
         graph.SetMarkerColor(color_list[i])
         graph.SetMarkerStyle(marker_list[i])
         eff_list.append(graph)
         graph.Draw(" same p")
-        save_canvas(c_single, f"efficiency_tracking_{det}_{sign}_{var}_{had}")
-        c_all.cd()
-        graph.Draw(" same p")
         leg_all.AddEntry(eff_list[i], had, "p")
+
     leg_all.Draw()
-    save_canvas(c_all, f"efficiency_tracking_alltogether_{det}_{sign}_{var}")
+    save_canvas(c_all, f"efficiency_tracking_{det}_{sign}_{var}")
 
 
 def efficiency_hadron(had, var):
@@ -152,6 +217,27 @@ def efficiency_hadron(had, var):
     save_canvas(ceffhf, f"efficiency_hfcand_{had}_{var}")
 
 
+def plot_selections(infile):
+    """
+    Plot track and MC particle selections.
+    """
+    track_sel = infile.Get("qa-efficiency/MC/trackSelection")
+    part_sel = infile.Get("qa-efficiency/MC/particleSelection")
+
+    canv = TCanvas("Track selection", "Track selection")
+    canv.SetCanvasSize(800, 600)
+    canv.cd()
+    track_sel.Draw()
+
+    canv2 = TCanvas("Particle selection", "Particle selection")
+    canv2.SetCanvasSize(800, 600)
+    canv2.cd()
+    part_sel.Draw()
+
+    save_canvas(canv, "efficiency_tracking_track_selection")
+    save_canvas(canv2, "efficiency_tracking_particle_selection")
+
+
 def main():
     """
     Main function.
@@ -160,33 +246,50 @@ def main():
 
     parser = argparse.ArgumentParser(description="Arguments to pass")
     parser.add_argument("input_file", help="input AnalysisResults.root file")
-    parser.add_argument("--dump_eff", default=False, action="store_true",
-                        help="Dump efficiency tree")
+    parser.add_argument(
+        "--plot_erry",
+        default=False,
+        action="store_true",
+        help="Plot efficiency with y error bars",
+    )
+    parser.add_argument(
+        "--plot_sel",
+        default=False,
+        action="store_true",
+        help="Plot track and particle selections",
+    )
+    parser.add_argument(
+        "--dump_eff", default=False, action="store_true", help="Dump efficiency tree"
+    )
     args = parser.parse_args()
 
     var_list = ["Pt", "Eta", "Phi"]
-    sign_list = ["Positive", "Negative"]
+    sign_list = ["Positive", "Negative", "All"]
     det_list = ["ITS-TPC", "ITS-TPC-TOF"]
 
     infile = TFile(args.input_file)
+    heff = infile.Get("qa-efficiency/EfficiencyMC")
 
     if args.dump_eff:
-        heff = infile.Get("qa-efficiency/EfficiencyMC")
         heff.Print()
         for i in range(heff.GetEntries()):
             teff = heff.At(i)
             teff.Print()
 
+    if args.plot_sel:
+        plot_selections(infile)
+
     for var in var_list:
         for sign in sign_list:
             for det in det_list:
-                efficiency_tracking(infile, det, sign, var)
+                efficiency_tracking(infile, heff, det, sign, var, args.plot_erry)
     for sign in sign_list:
-        efficiency_tracking(infile, "ITS", sign, "Pt")
+        efficiency_tracking(infile, heff, "ITS", sign, "Pt", args.plot_erry)
 
-    #hfhadron_list = ["d0", "dplus", "lc", "xic", "jpsi"]
-    #for had in hfhadron_list:
+    # hfhadron_list = ["d0", "dplus", "lc", "xic", "jpsi"]
+    # for had in hfhadron_list:
     #    efficiency_hadron(had, "Pt")
+
 
 if __name__ == "__main__":
     main()
